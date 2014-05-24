b2d26cade4f087bfa3742f31f27699c1e0a70501
fergunet <fergunet@gmail.com>
2014-05-24 18:51:41 +0200

    First draft using Bootstrap template

---
+++ cfps.php
@@ -0,0 +1,29 @@
+<div class="page-header">
+        <h2>Call for papers:<h2>
+</div>
+
+
+<a href="eurogp.php">EuroGP</a><br/>
+EvoBIO<br/>
+EvoCOP<br/>
+EvoMUSART<br/>
+EvoApplications<br/>
+<ul>
+	<li>EvoCOMNET</li>
+<li>EvoCOMPLEX</li>
+<li>EvoENERGY</li>
+<li>EvoFIN</li>
+<li>EvoGAMES</li>
+<li>EvoHOT</li>
+<li>EvoIASP</li>
+<li>EvoINDUSTRY</li>
+<li>EvoNUM</li>
+<li>EvoPAR</li>
+<li>EvoRISK</li>
+<li>EvoROBOT</li>
+<li>EvoSTOC</li>
+</ul>
+EvoStar GENIL Award <br/>
+EvoStar Best Paper Candidates<br/>
+EvoStar Best Paper Awards 2014<br/>
+</ul>
---
+++ index.php
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+<?php include('header.php') ?>
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Evostar 2015</h1>
+        <p>The Leading European Event on Bio-Inspired Computation.</p>
+        <p>Copenhagen, Denmark, 8-10 April 2015</p>
+      </div>
+
+<div class="row">
+    <div class="col-sm-3">
+      <?php include('cfps.php') ?>
+    </div>
+
+    <div class="col-sm-6">
+      <div class="page-header">
+        <h2>Latest news:<h2>
+      </div>
+
+    </div>
+
+    <div class="col-sm-3">
+      <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Important dates:</h3>
+            </div>
+            <div class="panel-body">
+              Submission Deadline
+            </div>
+      </div>
+      <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Twitter:</h3>
+            </div>
+            <div class="panel-body">
+              Tweets:
+            </div>
+      </div>
+
+    </div>
+</div>
+
+<?php include('footer.php') ?>
+
+
