77c263c07f99c9537424def239b4e2fca1f7fb1f
fergunet <fergunet@gmail.com>
2014-06-18 18:01:13 +0200

    Added organisers page and photos and css

---
+++ organisers.php
@@ -0,0 +1,158 @@
+<?php include('header.php') ?>
+
+        <h2>Evostar 2015 Organizers<h2>
+   </div>
+
+
+<h3>EuroGP</h3>
+<h4>18th European Conference on Genetic Programming</h4>
+ 
+<strong>EuroGP programme chairs :</strong>
+
+<table class="organisers">
+<tr>
+	<td><img src="organisers/penousal_machado.jpg" width="100"></td>
+	<td><img src="organisers/malcolm_heywood.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Penousal Machado, University of Coimbra, Portugal, machado@dei.uc.pt</td>
+	<td>Malcolm Heywood, Dalhousie University, Canada, mheywood@cs.dal.ca</td>
+</tr>
+</table>
+
+
+
+<strong>EuroGP publication chair :</strong>
+<table class="organisers">
+<tr>
+	<td><img src="organisers/james_mcdermott.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>James McDermott,  University College Dublin, Ireland,  jamesmichaelmcdermott(at)gmail.com</td>
+	
+</tr>
+</table>
+
+
+ 
+ 
+<h3>EvoCOP</h3>
+<h4>15th European Conference on Evolutionary Computation in Combinatorial Optimisation</h4>
+ 
+<strong>EvoCOP programme chairs :</strong>
+
+<table class="organisers">
+<tr>
+	<td><img src="organisers/gabriela_ochoa.jpg" width="100"></td>
+	<td><img src="organisers/jose_francisco_chicano.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Gabriela Ochoa, University of Stirling, UK, gabriela.ochoa@cs.stir.ac.uk</td>
+	<td>Francisco Chicano, University of Malaga, Spain, chicano@lcc.uma.es</td>
+</tr>
+</table>
+
+
+
+
+
+ 
+ 
+<h3>EvoMUSART</h3>
+<h4>4th International Conference on Evolutionary and Biologically Inspired Music, Sound, Art and Design</h4>
+ 
+<strong>EvoMUSART programme chairs :</strong>
+
+<table class="organisers">
+<tr>
+	<td><img src="organisers/colin_johnson.png" width="100"></td>
+	<td><img src="organisers/adrian_carballal.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Colin Johnson, University of Kent, UK, c.g.johnson@kent.ac.uk</td>
+	<td>Adrián Carballal, University of A Coruña, Spain,  adriancarballal@gmail.com</td>
+</tr>
+</table>
+
+
+
+ 
+<strong>EvoMUSART Publication chair :</strong>
+<table class="organisers">
+<tr>
+	<td><img src="organisers/joao_correia.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Jo&atilde;o Correia, University of Coimbra, Portugal, jncor@dei.uc.pt</td>
+</tr>
+</table>
+
+
+
+ 
+ 
+<h3>EvoAPPLICATIONS</h3>
+<h4>17th European Conference on the Applications of Evolutionary Computation</h4>
+<strong>EvoApplications coordinator :</strong>
+
+<table class="organisers">
+<tr>
+	<td><img src="organisers/antonio_mora.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Antonio Mora, University of Granada, Spain,  amorag@geneura.ugr.es</td>
+</tr>
+</table>
+ 
+<strong>EvoAPPS publication chair :</strong>
+
+
+<table class="organisers">
+<tr>
+	<td><img src="organisers/giovanni_squillero.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Giovanni Squillero, Politecnico di Torino, Italy, giovanni.squillero@polito.it</td>
+</tr>
+</table>
+ 
+<h3>EvoStar 2015 publicity chairs</h3>
+<table class="organisers">
+<tr>
+	<td><img src="organisers/pablo_garcia.jpg" width="100"></td>
+	<td><img src="organisers/mauro_castelli.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Pablo García Sánchez, University of Granada, Spain, pablogarcia@ugr.es</td>
+	<td>Mauro Castelli, ISEGI; Universidade Nova de Lisboa, Portugal, castelli.mauro@gmail.com</td>
+</tr>
+</table>
+ 
+<h3>EvoStar 2015 local chairs</h3>
+
+
+
+<table class="organisers">
+<tr>
+	<td><img src="organisers/paolo_burelli.jpg" width="100"></td>
+	<td><img src="organisers/julian_togelius.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Paolo Burelli, Aalborg University, Copenhagen, Denmark, pabu@create.aau.dk</td>
+	<td>Julian Togelius, IT University of Copenhagen, Denmark, juto@itu.dk</td>
+</tr>
+</table>
+ 
+ 
+<h3>EvoStar coordinator</h3>
+<table class="organisers">
+<tr>
+	<td><img src="organisers/jennifer_willies.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Jennifer Willies, Edinburgh Napier University, UK, j.willies@napier.ac.uk</td>
+</tr>
+</table>
+
+
+<?php include('footer.php') ?>
