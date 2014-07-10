9620f338822bdce5621118a8059e2a08ea2e4186
fergunet <fergunet@gmail.com>
2014-07-10 22:42:48 +0200

    Evoapps flyer and modifications of EvoCop

--- index.php
+++ index.php
@@ -13,7 +13,9 @@
 <p>Welcome to Evo<span class="red">star</span> 2015 webpage.<p> 
 	<p>This year, the conference will be held in Copenhagen, Denmark, from 8 to 10 April 2015. Read more about Evo<span class="red">star</a> <a href="about_evostar.php">here</a>.</p>
 
-<p><a href="evo2015.pdf" target="_blank">Download the EvoStar 2015 flyer</a>.</p>
+<h3>Flyers</h3>
+<p><a href="evo2015.pdf" target="_blank">EvoStar 2015 flyer</a>.</p>
+<p><a href="evoAPPS.pdf" target="_blank">EvoApps 2015 flyer</a>.</p>
 <?php include('footer.php') ?>
 
 
