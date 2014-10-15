62b601f5fbeefcb8a2831d26a75a388b9c89a13a
fergunet <fergunet@gmail.com>
2014-10-15 14:10:52 +0200

    Added BIO, submission_details, and modifications in the rest: titles, PCs, etc.

---
+++ submision_details.php
@@ -0,0 +1,19 @@
+ <?php include('header.php') ?>
+
+        <h2>Submission <span class="red">details</span></h2>
+        </div>
+        Information about formatting and submission process is available in each conference webpage (left menu).
+
+        <h3>Submission links</h3>
+
+    <ul>
+        <li>Submission link for Euro<span class="red">GP</span>: <a href="http://myreview.csregistry.org/eurogp15/">http://myreview.csregistry.org/eurogp15/</a></li>
+        <li>Submission link for Evo<span class="red">COP</span>: <a href="http://myreview.csregistry.org/evocop15/">http://myreview.csregistry.org/evocop15/</a></li>
+        <li>Submission link for Evo<span class="red">MUSART</span>: <a href="http://myreview.csregistry.org/evomusart15/">http://myreview.csregistry.org/evomusart15/</a></li>
+        <li>Submission link for Evo<span class="red">APPS</span>: <a href="http://myreview.csregistry.org/evoapps15/">http://myreview.csregistry.org/evoapps15/</a></li>
+    </ul>
+
+
+
+
+    <?php include('footer.php') ?>
