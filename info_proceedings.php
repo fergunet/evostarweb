a8e1fa96bbf654c9bc1d68b843c12dea3f5f8769
fergunet <fergunet@gmail.com>
2014-10-20 12:09:31 +0200

    Letter of apology

---
+++ info_proceedings.php
@@ -0,0 +1,10 @@
+<?php include('header.php') ?>
+
+        <h2>Letter of apology <h2>
+   </div>
+Springer advise that the EvoAPPS 2014 proceedings will be available for download in early December and all EvoStar2014  authors will receive notification details as soon as these are available.  Our apologies for the serious delay in this year's publication of conference proceedings.
+
+<p align="right">
+<i>The Evostar 2014 organizers.</i>
+</p>
+    <?php include('footer.php') ?>
