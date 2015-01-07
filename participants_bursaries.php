5f0be6da184efd25c2866887e32eb129aa33bdce
fergunet <fergunet@gmail.com>
2015-01-07 18:14:07 +0100

    Added letter, bursaries and registration information

---
+++ participants_bursaries.php
@@ -0,0 +1,28 @@
+<?php include('header.php') ?>
+
+        <h2>Travel and accommodation bursaries<h2>
+   </div>
+
+<h3>Application deadline:  25 February 2015</h3>
+
+<p> 
+A limited number of travel bursaries &amp; pre-paid accommodation places will be available for those whose papers have been accepted for EuroGP, EvoCOP, EvoMusArt and EvoApplications 2015.  Our intention is to offer assistance to as many students as possible and <strong>applications will only be accepted from those who have already registered for the conferences/workshops</strong>.  Do not apply for an EvoStar bursary until you have completed the registration process.  Priority will be given to post-graduate students whose EvoStar papers have been accepted, especially those travelling transcontinental journeys or from countries outside Europe facing real financial hardship. Providing part-support for conference travel or accommodation costs is meant to make it easier for individuals to obtain additional funding from their own institution, or from other sources.
+ </p>
+
+ <p>
+There are two types of financial support:  for <strong>travel</strong> and for <strong>accommodation</strong>.
+ </p>
+ <p>
+<strong>Travel bursaries</strong> are principally for students travelling inter-continental distances and are limited to a maximum 500 Euro, but the amount awarded may be less.  They are claimed retrospectively and original travel tickets/boarding passes, hotel or similar receipts must be submitted after the conferences for reimbursement.
+ </p><p>
+<strong>Accommodation bursaries</strong> are for pre-paid bed and breakfast accommodation for three nights and are primarily aimed at students residing within Europe.  Hotel rooms near the conference venue in Copenhagen have been reserved and costs will be paid directly by the organisers.   Individuals prepared to share in twin accommodation rooms are given high priority, but some single rooms will also be available.  You can specify with whom you would like to share, and you will not be asked to share with anyone who is not of similar gender, age and nationality.   Please note that several strong EvoStar friendships and even research collaborations have resulted from students sharing accommodation!
+ </p><p>
+We are trying to support as many EvoStar participants as possible with limited funds so please be realistic in your expectations of the support that can be offered.
+ </p><p>
+If you wish to apply for travel bursary or an accommodation place, please complete asap the <a href="https://www.surveymonkey.com/s/EvoStarBursary">BURSARY APPLICATION FORM</a>.   If you have any queries, please email the <a href="mailto:j.willies@napier.ac.uk">EvoStar Conference Coordinator</a>  (including TRAVEL BURSARY QUERY in your email subject header).
+ </p><p>
+<strong>The deadline for bursary applications is 25 February 2015</strong> and decisions will be made by 5 March and sent to all applicants.
+</p>
+
+
+<?php include('footer.php') ?>
---
+++ participants_registration.php
@@ -0,0 +1,86 @@
+<?php include('header.php') ?>
+
+        <h2>Evo* 2015 REGISTRATION<h2>
+   </div>
+
+
+<h4><span class="red">Early Registration Discount Expires 1 March 2015</span></h4>
+
+<p><strong>Registration Closes 31 March 2015 (before Easter weekend)</strong></p>
+
+<p>Evo<span class="red">*</span> takes place on 8-10 April 2015 in Copenhagen, Denmark </p>
+
+</applet><strong><a href="">REGISTER HERE</a></strong>
+
+
+<p>Evo<span class="red">Star</span>2015 is the name for the four co-located conferences of Euro<span class="red">GP</span>, Evo<span class="red">COP</span>, Evo<span class="red">MusArt</span> and also Evo<span class="red">Applications</span> including conference tracks of Evo<span class="red">BIO</span>, Evo<span class="red">COMNET</span>, Evo<span class="red">COMPLEX</span>, Evo<span class="red">ENERGY</span>, Evo<span class="red">FIN</span>, Evo<span class="red">GAMES</span>, Evo<span class="red">IASP</span>, Evo<span class="red">INDUSTRY</span>, Evo<span class="red">NUM</span>, Evo<span class="red">PAR</span>, Evo<span class="red">RISK</span>, Evo<span class="red">ROBOT</span>, Evo<span class="red">STOC</span>.  These co-located events will be held in Copenhagen, Denmark on 8-10 April 2015.</p>
+
+<p>The single registration fee allows access to all events over three days and includes lunches and coffee breaks at the conference venue; one ticket for the conference reception and one ticket for the conference dinner (additional tickets available for purchase).  Also included is a USB stick with all EvoStar2015 conference proceedings and electronic access for these LNCS volumes will be available for a limited period before and after the EvoStar events via www.springerlink.com.  Hard copy volumes are <strong>NOT</strong> included as part of the registration fee but are available for separate purchase directly from Springer.</p>
+
+<p>The friendly and informal atmosphere at EvoStar is intended to foster beneficial friendships and longer-term collaboration across continents and across research generations.</p>
+
+<p><strong>The early booking discount is given to all registrations received by 1 March 2015</strong>, and you are encouraged to take advantage of the lower fees.  The reduced registration rate will automatically cease after 1 March and thereafter registrations will be charged at the higher rate (no exceptions). The organisers work hard to minimise conference fees, and the 2015 registration rates represent a decrease to previous rates in real terms.  Conference registrations are available on a first-come, first-served basis, with the organisers reserving the right to limit places if necessary. <strong>Registrations close on 31 March 2015</strong>, a week before the events commence and just before the Easter weekend. <strong>Half price student discounts are available</strong> for those registered at appropriate academic organisations, who are able to prove their student status if asked to do so.  </p>
+
+<p>Online registration and payment are available via a SSL secure website operated by Edinburgh Napier University in conjunction with Barclaycard.  Online payment can be made using VISA, MASTERCARD, VISA DEBIT, VISA ELECTRON.  Please note that payment is charged in British pounds sterling at the rates shown below, with Euro fees described as an approximate guide.  A different Euro rate may be charged by your bank or credit card company.  Payment can also be made by cheque in Euros or British pounds sterling (please contact <a href="j.willies@napier.ac.uk">Jennifer Willies</a>, EvoStar Conference Coordinator for more information).  <strong><span class="red">Please note that sending funds by bank transfer is not available for this conference.</span></strong>  </p>
+
+<h3><a href=""><p>Register HERE  </p></a></h3>
+
+
+<div class="panel panel-default">
+            <div class="panel-heading">
+              <h2 class="panel-title">Evostar 2015 registration</h2>
+            </div>
+            <div class="panel-body">
+              
+             <strong>WHAT REGISTRATION INCLUDES</strong>
+
+The SINGLE registration for all EVO* events includes:
+<ul>
+<li>Attendance at all EvoStar sessions for all four conferences </li>
+<li>Lunches and coffee breaks at the conference venue over three days</li>
+<li>One ticket for the conference reception </li>
+<li>One ticket for the conference dinner (additional tickets available for purchase)</li>
+<li>One USB stick including all EvoStar2015 conference proceedings</li>
+<li>Electronic access via www.springerlink.com for all four EvoStar2015 LNCS volumes for a limited period before and after the EvoStar events.  Please note that hard copies of the proceedings are NOT included in the registration fee.</li>
+
+</ul>
+</h3>REGISTRATION FEES</h3>
+
+Online payment is charged in GBP, and these Euro rates are approximate, and for general guidance only.
+
+
+<table class="table table-bordered">
+	<tr>
+		<td></td><td><strong><span class="red">EARLY Registration until 1 March 2015</span></strong></td><td>LATE Registration from 2-31 March 2015</td>
+	</tr>
+
+	<tr>
+		<td>Student</td><td class="success"><strong>200 GBP</strong> (approx 250 EUR)</td><td>300 GBP (approx 390 EUR)</td>
+	</tr>
+
+	<tr>
+		<td>Regular</td><td class="success"><strong>400 GBP</strong> (approx 500 EUR)</td><td>550 GBP (approx 700 EUR)</td>
+	</tr>
+</table>
+
+<h3>ADDITIONAL ITEMS YOU CAN ORDER</h3>
+
+Additional conference dinner tickets for accompanying persons may be purchased for 35 GBP (approx 43 EUR).
+
+
+<h3>HOW TO REGISTER AND PAY</h3>
+
+<p>
+Online registration and payment are available via a SSL secure website operated by Edinburgh Napier University in conjunction with Barclaycard.  Online payment can be made using VISA, MASTERCARD, VISA DEBIT, VISA ELECTRON.  Please note that payment is charged in British pounds sterling at the rates shown below, with Euro fees described as an approximate guide.  A different Euro amount may be charged by your bank or credit card company.  Payment can also be made by cheque in Euros or British pounds sterling (please contact <a href="mailto:j.willies@napier.ac.uk">Jennifer Willies, EvoStar Conference Coordinator</a> for more information). <strong> <span class="red"> Please note that sending funds by bank transfer is NOT available for EvoStar, payment only by credit card or EUR/GBP cheque.</span> </strong> 
+</p>
+<p>
+<a href="">Register for EvoStar 2015 Now  </a>
+</p>
+
+<h3>FURTHER INFORMATION</h3>
+From <a href="mailto:j.willies@napier.ac.uk">Jennifer Willies</a>, EvoStar Conference Coordinator 
+              
+            </div>
+      </div>
+
+<?php include('footer.php') ?>
