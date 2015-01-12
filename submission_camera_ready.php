db3d7ca5479c649c2ef0d0e581bdedd8528de38e
fergunet <fergunet@gmail.com>
2015-01-12 19:05:47 +0100

    Added camera ready info

---
+++ submission_camera_ready.php
@@ -0,0 +1,123 @@
+<?php include('header.php') ?>
+
+        <h2>Camera ready instructions<h2>
+   </div>
+
+Please follow Springer's LNCS instructions when preparing your paper.
+
+<a href="http://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0">http://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0</a>
+There are strict page limits for each of the 4 Tracks collectively known as Evo*<br/>
+
+<strong>12 pages for Euro<span class="red">GP</span>, Evo<span class="red">COP</span>, Evo<span class="red">MUSART</span> & Evo<span class="red">Applications</span></strong>
+<br/>
+
+Please ensure that submissions are directed to the relevant track. The corresponding contact editors for copyright forms are listed below.
+
+<ul>
+<li>Euro<span class="red">GP</span>	Malcolm Heywood: mheywood@cs.dal.ca</li>
+<li>Evo<span class="red">COP</span>	Gabriela Ochoa: gabriela.ochoa@cs.stir.ac.uk</li>
+<li>Evo<span class="red">MUSART</span>	Colin Johnson: c.g.johnson@kent.ac.uk</li>
+<li>Evo<span class="red">Applications</span>	Antonio Mora: amorag@geneura.ugr.es</li>
+ </ul>
+
+Papers which are not formatted according to those guidelines, or do not respect the page limit, risk being excluded from the proceedings.
+
+Submit your final camera-ready paper NO LATER than
+
+<strong><span class="red">21 January 2015</span></strong>
+
+through the appropriate submission system at
+
+<ul>
+<li>Euro<span class="red">GP</span>: <a href="http://myreview.csregistry.org/eurogp15/SubmitPaper.php">http://myreview.csregistry.org/eurogp15/SubmitPaper.php</a></li>
+<li>Evo<span class="red">COP</span>: <a href="http://myreview.csregistry.org/evocop15/SubmitPaper.php">http://myreview.csregistry.org/evocop15/SubmitPaper.php</a></li>
+<li>Evo<span class="red">MUSART</span>: <a href="http://myreview.csregistry.org/evomusart15/SubmitPaper.php">http://myreview.csregistry.org/evomusart15/SubmitPaper.php</a></li>
+<li>Evo<span class="red">Applications</span>: <a href="http://myreview.csregistry.org/evoapps15/SubmitPaper.php">http://myreview.csregistry.org/evoapps15/SubmitPaper.php</a></li>
+</ul>
+using your id and password.
+
+<p>
+Springer reprocesses all the contributions, meaning that you have to provide all source files and figures.
+</p>
+
+You should prepare a single tgz or zip-file containing a single directory with all of the necessary files (and all the necessary .sty files).
+
+The name of the directory should be the last name of the first
+author.
+
+The directory has to contain:
+
+<ul>
+<li><strong>Latex .tex file</strong> (named with the last name of the first author).</li>
+
+<li><strong>Latex .bbl file</strong>, if references are in a separate file (named with the last name of the first author).</li>
+
+<li><strong>All figures</strong> (.ps or .eps).</li>
+
+<li><strong>Final .dvi file</strong> (named with the last name of the first author).</li>
+
+<li><strong>Final .ps file</strong> (named with the last name of the first author).</li>
+
+<li><strong>Final .pdf file</strong> (named with the last name of the first author).</li>
+
+<li><strong>.rtf file</strong> if another word processing program other than Latex was used (named with the last name of the first author).</li>
+
+<li><strong>authors.txt</strong> : This file should have one line for each author of the paper using the following format:
+
+<br/>
+<em>[first names of first author] [last name of first author] [email]<br/>
+[first names of second author] [last name of second author] [email]<br/></em>
+<br/>
+Please mark the contact author with * at the end of the line.<br/><br/>
+
+Example of a paper authored by Mario Rossi and Jos'e Salgado:<br/>
+<em>Mario Rossi mario.rossi@myuni.xy<br/>
+Jos'e Salgado jsagado@hisuni.yx*<br/></em>
+</li>
+<li><strong>abstract.txt</strong> : This file should have the following structure:</li>
+
+first line: title of the paper<br/>
+second line: blank<br/>
+remaining lines: abstract of the paper (in ASCII without line breaks).</br>
+<li><strong>Scanned signed copyright</strong> form as PDF using the following filename:</li>
+
+<em>[last_name_of_first_author]cr.pdf<br/></em>
+(one author may sign on behalf of all authors of a paper).<br/>
+Please note that the title of the conference should be entered as either:<br/>
+Euro<span class="red">GP</span> 2015, Evo<span class="red">COP</span> 2015 Evo<span class="red">MUSART</span> 2015 or Evo<span class="red">Applications</span> 2015
+</li>
+</ul>
+Also, before <strong>21 January 2015</strong>, you also have to send an <strong>E-mail</strong> to j.willies@napier.ac.uk with the title:
+
+<p>
+"EvoStar15 attendance confirmation"<br/>
+which includes the following information:<br/>
+1. Name of (co)author who will attend EvoStar2015 ________<br/>
+2. If attendee is a student, their advisor ___________ has been cc-ed into this mail<br/>
+3. If a VISA is required for the attendee, the application process has started: Not-required / Yes / No.<br/>
+</p>
+
+<p>
+When preparing your camera-ready, please make sure that you omit all headers (no page numbers, no running title, no running
+authors) but specify them in the .tex source, because Springer will
+activate them in reformatting the proceedings. For Word users, add the
+specifications without activating headers.
+</p>
+
+<p>Authors not following the instructions may result in the paper being excluded from the proceedings.</p>
+
+The copyright form is available from:
+
+<a href="http://www.springer.com/cda/content/document/cda_downloaddocument/LNCS_Copyright_Form?SGWID=0-0-45-154182-0">http://www.springer.com/cda/content/document/cda_downloaddocument/LNCS_Copyright_Form?SGWID=0-0-45-154182-0</a>
+
+<hr/>
+In summary, you have to do each of the following 5 tasks no later than <strong>21 January 2015</strong>
+<ul>
+<li>Submit the paper including all source files (see above for details)</li>
+<li>Submit an authors.txt file</li>
+<li>Submit an abstract.txt file</li>
+<li>Complete and submit a signed copyright form as PDF</li>
+<li>Send an email with the name of the (co)author who will attend and register at Evo<span class="red">Star</span> 2015</li>
+<li>At least one author must register and pay the conference fee by the <strong>15 February</strong> 2015. Failure to do so could result in the paper being excluded from publication.</li>
+</ul>
+    <?php include('footer.php') ?>
