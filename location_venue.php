417b47e7a881d1ae2aac1e7a8c27fbd5fce4ba26
fergunet <fergunet@gmail.com>
2015-01-20 11:04:31 +0100

    Added location venue

---
+++ location_venue.php
@@ -0,0 +1,66 @@
+
+<?php include('header.php') ?>
+
+        <h2>Venue <span class="red">Location</span><h2>
+   </div>
+
+
+
+<div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Venue address</h3>
+            </div>
+            <div class="panel-body">
+            <p><strong>Location</strong>: National Museum / Nationalmuseet,  Ny Vestergade 10  1471 Copenhagen</p>
+            <p><strong><a href="https://maps.google.dk/maps?q=Nationalmuseet,+Ny+Vestergade,+K%C3%B8benhavn&hl=da&ie=UTF8&sll=56.23008,11.5425&sspn=5.156185,14.27124&oq=nationalmu&hq=Nationalmuseet,+Ny+Vestergade,+K%C3%B8benhavn&t=m&z=14&output=classic&dg=brw">Google Maps Link</a></strong></p>
+            <p><strong>Conference times:</strong>
+			9am Wed 8 April through Fri 10 April with Friday afternoon local excursion.
+			</p>
+
+<strong>More information</strong>: <a href="http://natmus.dk/en/the-national-museum-of-denmark/">http://natmus.dk/en/the-national-museum-of-denmark/</a>
+
+
+
+
+
+			</div>
+</div>
+
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Nationalmuseet,+Ny+Vestergade,+K%C3%B8benhavn&amp;hl=da&amp;ie=UTF8&amp;sll=56.23008,11.5425&amp;sspn=5.156185,14.27124&amp;oq=nationalmu&amp;hq=Nationalmuseet,+Ny+Vestergade,+K%C3%B8benhavn&amp;t=m&amp;ll=55.674648,12.574741&amp;spn=0.006295,0.006295&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?q=Nationalmuseet,+Ny+Vestergade,+K%C3%B8benhavn&amp;hl=da&amp;ie=UTF8&amp;sll=56.23008,11.5425&amp;sspn=5.156185,14.27124&amp;oq=nationalmu&amp;hq=Nationalmuseet,+Ny+Vestergade,+K%C3%B8benhavn&amp;t=m&amp;ll=55.674648,12.574741&amp;spn=0.006295,0.006295&amp;source=embed" style="color:#0000FF;text-align:left">Vis stort kort</a></small>
+
+
+<p>
+Evo<span class="red">Star</span> 2015 will be held at the <stron>National Museum of Denmark</strong> in the centre of 
+
+Copenhagen which is housed in an 18th century palace.  
+
+Opening at 9am on Wednesday, 8 April with invited speaker <a href="speaker_oudeyer.php">Pierre-Yves 
+
+Oudeyer</a>, EvoStar runs until 
+
+lunchtime on Friday, 10 April following the closing talk by invited speaker  
+
+ <a href="speaker_hogeweg">Paulien Hogeweg</a>,  The programme 
+
+will be posted at the website by early February.
+</p>
+
+<p>
+A local social trip will take place on Friday afternoon with details to be 
+
+announced shortly.
+</p>
+
+<p>
+The National Museum of Denmark offers a wealth of historical exhibits and these 
+
+are open to all EvoStar participants.
+
+Good AV facilities are available including WIFI but authors should bring their own 
+
+laptops/tablets to present their work, or make arrangements with colleagues if 
+
+bringing presentations on a USB stick.
+</p>
+
+    <?php include('footer.php') ?>
