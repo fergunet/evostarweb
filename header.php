8f7c5f6cad8fcabd1a7fa4a477eb17bd2328819d
fergunet <fergunet@gmail.com>
2015-01-31 15:29:12 +0100

    Added speakers

--- header.php
+++ header.php
@@ -60,7 +60,7 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Programme<b class="caret"></b></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Invited Speakers</a></li>
+                <li><a href="programme_speakers.php">Invited Speakers</a></li>
                 <li><a href="#">Overview programme</a></li>
                 <li><a href="#">Detailed programme</a></li>
                 <li><a href="#">Best Paper Candidates</a></li>
---
+++ programme_speakers.php
@@ -0,0 +1,28 @@
+<?php include('header.php') ?>
+
+        <h2>Invited Speakers<h2>
+   </div>
+
+
+<h2>
+	Open-Source Baby Robots for Science, Education and Art, by <span class="red">Pierre-Yves Oudeyer</a></h2>
+
+
+<p>
+<strong>Abstract:</strong> What can baby robots tell us about ourselves? Mysteries of human cognition, like the mechanisms of curiosity or the origins of languages, are starting to be unveiled through experiments with robots that can learn by themselves. In this talk, I will present several examples of such research projects, with models of curiosity-driven learning or language formation. I will emphasize the importance of understanding and experimenting the role of the body in cognition, and in this context introduce the Poppy open-source 3D printed humanoid robot. I will illustrate how Poppy allows rapid experimentation of varied robotic morphologies. I will also explain how this platform, born inside a scientific project, also opens stimulating opportunities in the world of education, for the integrated and interdisciplinary learning of mechanics, 3D printing, electronics, computer science, artificial intelligence and design.
+I wil also give examples of projects exploring how it can be used in the artistic domain.
+</p>
+
+<div>
+
+	<img src="img/pierre.jpg" width="200px" align="right" style="margin:10px"/>
+
+<p>
+<strong>Bio:</strong> <em>Dr. Pierre-Yves Oudeyer is Research Director at Inria and head of the Inria and Ensta-ParisTech FLOWERS team (France). Before, he has been a permanent researcher in Sony Computer Science Laboratory for 8 years (1999-2007). After working on computational models of language evolution, he is now working on developmental and social robotics, focusing on sensorimotor development, language acquisition and life-long learning in robots. Strongly inspired by infant development, the mechanisms he studies include artificial curiosity, intrinsic motivation, the role of morphology in learning motor control, human-robot interfaces, joint attention and joint intentional understanding, and imitation learning. He has published a book, more than 80 papers in international journals and conferences, holds 8 patents, gave several invited keynote lectures in international conferences, and received several prizes for his work in developmental robotics and on the origins of language. In particular, he is laureate of the ERC Starting Grant EXPLORERS. He is editor of the IEEE CIS Newsletter on Autonomous Mental Development, and associate editor of IEEE Transactions on Autonomous Mental Development, Frontiers in Neurorobotics, and of the International Journal of Social Robotics. He is also working actively for the diffusion of science towards the general public, through the writing of popular science articles and participation to radio and TV programs as well as science exhibitions. Web: <a href="http://www.pyoudeyer.com">http://www.pyoudeyer.com</a> and <a href="http://flowers.inria.fr">http://flowers.inria.fr</a></em>
+</p>
+</div>
+
+<h2><span class="red">Paulien Hogeweg</span></h2>
+<em>Paulien Hogeweg is a Dutch theoretical biologist and complex systems researcher studying biological systems as dynamic information processing systems at many interconnected levels.  Together with Ben Hesper he coined the term "bioinformatics" in the 70's, as the study of informatics processes in biotic systems, including both data analysis and modelling.  Currently her main interest is studying "multilevel evolution": how new levels of selection emerge, and how  complex genotype-phenotype transitions evolve.</em>
+
+       <?php include('footer.php') ?>
