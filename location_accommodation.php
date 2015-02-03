aab198cd4e1e5d02e25564baf36f54f89c92d832
fergunet <fergunet@gmail.com>
2015-02-03 17:02:22 +0100

    Location information added and updated

---
+++ location_accommodation.php
@@ -0,0 +1,43 @@
+
+<?php include('header.php') ?>
+
+        <h2>Accommodation <span class="red">Information</span><h2>
+   </div>
+
+<h3>
+Copenhagen hotels</h3>
+ <p>
+There are a number of hotels in the central area of Copenhagen at a range of prices.  EvoStar participants are encouraged to undertake online searches at travel booking websites for the best deals. 
+ </p>
+ <p>
+Here are some hotels near the conference venue:
+ </p>
+ <ul>
+<li>Ascot Hotel ****</li>
+<li>Best Western Hotel City ***</li>
+<li>Best Western Mercur Hotel ***</li>
+<li>Cabinn City **</li>
+<li>City Hotel Nebo **</li>
+<li>Copenhagen Downtown Hostel ***</li>
+<li>Copenhagen Plaza ****</li>
+<li>Copenhagen Strand Hotel ***</li>
+<li>Danhostel Copenhagen City</li>
+<li>First Hotel Mayfair ****</li>
+<li>First Hotel Twentyseven ****</li>
+<li>Generator Copenhagen **</li>
+<li>Grand Hotel ****</li>
+<li>Hotel Alexandra ***</li>
+<li>Hotel Ansgar ***</li>
+<li>Hotel Danmark ***</li>
+<li>Hotel Maritime ***</li>
+<li>Hotel Opera ***</li>
+<li>Hotel Tiffany ***</li>
+<li>Imperial Hotel ****</li>
+<li>Marriott Copenhagen *****</li>
+<li>Saga Hotel **</li>
+<li>Scandic Palace Hotel ****</li>
+<li>The Square ****</li>
+<li>Villa Armonia Guest Rooms **</li>
+<li>Wakeup Copenhagen Carsten Niebuhrs Gade **</li>
+</ul>
+    <?php include('footer.php') ?>
---
+++ location_copenhagen.php
@@ -0,0 +1,206 @@
+<?php include('header.php') ?>
+
+        <h2>Getting About <span class="red">Copenhagen</span><h2>
+   </div>
+
+   <h3>How To Get To Copenhagen:</h3>
+
+<h4>By Plane:</h4>
+
+Copenhagen's airport has direct flights from many cities within Europe
+and from some within the United States
+(<a href="http://www.cph.dk/en/flight-info/route-map/">http://www.cph.dk/en/flight-info/route-map/</a>). The airport is located
+on the island of Amager, which is a 20 minute Metro ride away from the
+centre.
+
+<h4>By Train:</h4>
+
+Copenhagen can also conveniently be reached by direct internal trains
+from Hamburg, Stockholm, Basel, and Amsterdam, among other
+(<a href="http://www.dsb.dk/om-dsb/in-english/">http://www.dsb.dk/om-dsb/in-english/</a>). The Copenhagen Central Station
+is around a 10 minute walk away from the conference venue.
+
+<h3>From Copenhagen Airport to the Centre:</h3>
+
+<h4>Metro:</h4>
+<p>
+After you exit the baggage reclaim area, keep walking straight to the
+end of the terminal building. There are two elevators and a staircase
+that lead to the airport metro station, which is located at the upper
+level. To get to the city centre you will need to buy a ticket from
+the DSB office (see below).
+The airport is the terminal station of the M2 line, which means you
+can board any metro. Make sure you are at the Metro station though;
+there are other trains that leave from the airport.
+</p>
+<p>
+If you have a hotel near the conference venue you should get off at
+Kongens Nytorv station; it is approximately 10-15 mins walk to the
+venue from Kongens Nytorv. Alternatively, you may take the A1 towards
+Avedøre St. from Kongens Nytorv and exit at "Stormbroen,
+Nationalmuseet" (3 stops).
+</p>
+<h4>Taxi:</h4>
+A taxi ride from the airport to the city centre takes about 20 minutes.
+
+A convenient resource for travel planning in and around Copenhagen is
+Rejseplanen (<a href="http://www.rejseplanen.dk/">http://www.rejseplanen.dk/</a>).
+
+<h3> Bicycle</h3>
+
+
+
+Bicycles are by far the most popular and convenient mean of transportation in 
+
+Copenhagen. There are more than 300km of bicycle lanes and many guided 
+
+bicycle tours.
+
+Bikes can be rented in almost all bike shops; moreover, a public bike sharing 
+
+service called City Bike is also active in the city
+
+<h4>Copenhagen City Bike</h4>
+
+The first time you use the city bike you rent the bike by entering your credit card 
+
+information and unlock the city bike on the small tablet which is located on the 
+
+handlebars. The bikes can be returned to any station in the system.
+
+You can register yourself as a user online before your rental and save some time.
+
+<h3>Public Transportation</h3>
+
+The public transportation in Copenhagen is very reliable, punctual, and it takes 
+
+you everywhere. In Copenhagen the trains, Metro and busses (including 
+
+waterbuses) can be accessed with the same ticket. All you need to know is how 
+
+many zones you will pass on your journey. Note the Metro will take you from the 
+
+airport to central Copenhagen in only 20 minutes.
+
+<h4>Tickets</h4>
+
+You can buy different kinds of tickets when using public transportation in 
+
+Copenhagen. It all depends on how long you are staying in the city, and how 
+
+much you are planning on using public transportation.
+
+Always buy a ticket before boarding the train or metro. On-board the buses, you 
+
+also have the possibility to buy your ticket from the driver, if you bring small 
+
+change. The same ticket can be used in the metro, bus, train and harbour bus. 
+
+Always have your ticket with you in case a conductor wants to see it. Travelling 
+
+without a valid ticket will result in a fine.
+
+<p>
+
+The most convenient options you have for the tickets are:
+</p>
+
+<ul>
+<li><strong>72-hour City Pass in zones 1-4</strong>
+
+The City Pass gives you unlimited access to buses, trains, metro and 
+
+harbour buses in zones 1, 2, 3 and 4, which includes the centre of 
+
+Copenhagen and to and from the airport.
+
+A adult 72-hour City Pass costs DKK 200. Buy this ticket at the airport, 
+
+station or buy it here.</li>
+
+<li><strong>Flexcard - 7 days </strong>
+
+You can also purchase a 7-day unlimited travel card called a Flexcard. It 
+
+gives you unlimited travel in your choice of zones (1-2). The price is DKK 
+
+250. The card is available for longer periods of time as well and is sold in 
+
+ticket sales at the stations (also at the airport).</li>
+
+<li><strong>72-hour Copenhagen Card</strong>
+
+A Copenhagen Card gives you unlimited travel on buses, trains, metro and 
+
+harbour buses in all zones in the capital region. In addition, you get free 
+
+entrance to 72 major attractions and museums as well as discounts in 
+
+several restaurants, sights, car rental and more.
+
+An adult 72-hours Copenhagen Card costs approximately DKK 580</li>
+</ul>
+
+
+<h4>Buses</h4>
+
+The A-buses are the primary buses in central Copenhagen. They drive every 3-7 
+
+minutes during rush hour (which is between 07:00-09:00 in the morning and 
+
+15:30-17:30 in the afternoon) and usually about every 10 minutes before and 
+
+after rush hour. The A-buses serve at all hours.
+
+The S-buses drive every 5-10 minutes during rush hour and every 20 minutes 
+
+outside of rush hour. The S-buses are faster than the A-busses due to fewer 
+
+stops. They usually serve between 06:00 in the morning and 01:00 at night.
+
+Night buses are in service between 01:00 and 05:00 in the night. You can 
+
+recognise the bus stops by the grey colour of the bus stop signs. The night buses 
+
+are all N buses, for an example 85N.
+
+<h4>Metro</h4>
+
+The metro is in service all day and all night, every day of the week.
+
+There are 2-4 minutes between each train during rush hour, and 3-6 minutes 
+
+outside rush hour and during the weekends. Friday and Saturday night (after 
+
+01:00) trains arrive with a 7-15 minutes interval and a 20 minutes interval after 
+
+midnight on weekdays (Sunday through Thursday).
+
+Please note that maintenance of the rails may occur during the night, which can 
+
+result in a slightly longer journey. The maintenance is between 01:00-01:25 and 
+
+again between 04:15-04:45.
+
+<h4>Trains</h4>
+
+The S-trains run between 05:00 in the morning and 00:30 at night. Line F runs 
+
+every 4-5 minutes, line A, B, C and E run every 10 minutes, and line H and Bx run 
+
+every 20 minutes.
+
+On Friday and Saturday the trains run once an hour between 01:00 and 05:00. 
+
+However, line F runs every half hour during these hours.
+
+<h3>Useful links</h3>
+
+<ul>
+<li>Official Website: <a href="http://www.visitcopenhagen.com/">http://www.visitcopenhagen.com/</a></li>
+
+<li>Public Transportation: <a href="http://www.visitcopenhagen.com/copenhagen/transportation/transport-and-around-copenhagen">http://www.visitcopenhagen.com/copenhagen/transportation/transport-and-around-copenhagen</a></li>
+
+<li>Bike Rental: <a href="http://www.visitcopenhagen.com/copenhagen/transportation/copenhagen-bike-rentals">http://www.visitcopenhagen.com/copenhagen/transportation/copenhagen-bike-rentals</a></li>
+</ul>
+    <?php include('footer.php') ?>
