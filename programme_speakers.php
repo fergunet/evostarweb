5533bffeec1019a0d536d596b949724be65bdd16
fergunet <fergunet@gmail.com>
2015-02-16 14:09:53 +0100

    Added speaker information

--- programme_speakers.php
+++ programme_speakers.php
@@ -5,7 +5,7 @@
 
 
 <h2>
-	Open-Source Baby Robots for Science, Education and Art, by <span class="red">Pierre-Yves Oudeyer</a></h2>
+	Open-Source Baby Robots for Science, Education and Art, by <span class="red">Pierre-Yves Oudeyer</span></h2>
 
 
 <p>
@@ -18,11 +18,36 @@ I wil also give examples of projects exploring how it can be used in the artisti
 	<img src="img/pierre.jpg" width="200px" align="right" style="margin:10px"/>
 
 <p>
-<strong>Bio:</strong> <em>Dr. Pierre-Yves Oudeyer is Research Director at Inria and head of the Inria and Ensta-ParisTech FLOWERS team (France). Before, he has been a permanent researcher in Sony Computer Science Laboratory for 8 years (1999-2007). After working on computational models of language evolution, he is now working on developmental and social robotics, focusing on sensorimotor development, language acquisition and life-long learning in robots. Strongly inspired by infant development, the mechanisms he studies include artificial curiosity, intrinsic motivation, the role of morphology in learning motor control, human-robot interfaces, joint attention and joint intentional understanding, and imitation learning. He has published a book, more than 80 papers in international journals and conferences, holds 8 patents, gave several invited keynote lectures in international conferences, and received several prizes for his work in developmental robotics and on the origins of language. In particular, he is laureate of the ERC Starting Grant EXPLORERS. He is editor of the IEEE CIS Newsletter on Autonomous Mental Development, and associate editor of IEEE Transactions on Autonomous Mental Development, Frontiers in Neurorobotics, and of the International Journal of Social Robotics. He is also working actively for the diffusion of science towards the general public, through the writing of popular science articles and participation to radio and TV programs as well as science exhibitions. Web: <a href="http://www.pyoudeyer.com">http://www.pyoudeyer.com</a> and <a href="http://flowers.inria.fr">http://flowers.inria.fr</a></em>
+<strong>Bio:</strong><em> Dr. Pierre-Yves Oudeyer is Research Director at Inria and head of the Inria and Ensta-ParisTech FLOWERS team (France). Before, he has been a permanent researcher in Sony Computer Science Laboratory for 8 years (1999-2007). After working on computational models of language evolution, he is now working on developmental and social robotics, focusing on sensorimotor development, language acquisition and life-long learning in robots. Strongly inspired by infant development, the mechanisms he studies include artificial curiosity, intrinsic motivation, the role of morphology in learning motor control, human-robot interfaces, joint attention and joint intentional understanding, and imitation learning. He has published a book, more than 80 papers in international journals and conferences, holds 8 patents, gave several invited keynote lectures in international conferences, and received several prizes for his work in developmental robotics and on the origins of language. In particular, he is laureate of the ERC Starting Grant EXPLORERS. He is editor of the IEEE CIS Newsletter on Autonomous Mental Development, and associate editor of IEEE Transactions on Autonomous Mental Development, Frontiers in Neurorobotics, and of the International Journal of Social Robotics. He is also working actively for the diffusion of science towards the general public, through the writing of popular science articles and participation to radio and TV programs as well as science exhibitions. Web: <a href="http://www.pyoudeyer.com">http://www.pyoudeyer.com</a> and <a href="http://flowers.inria.fr">http://flowers.inria.fr</a></em>
 </p>
 </div>
 
-<h2><span class="red">Paulien Hogeweg</span></h2>
-<em>Paulien Hogeweg is a Dutch theoretical biologist and complex systems researcher studying biological systems as dynamic information processing systems at many interconnected levels.  Together with Ben Hesper he coined the term "bioinformatics" in the 70's, as the study of informatics processes in biotic systems, including both data analysis and modelling.  Currently her main interest is studying "multilevel evolution": how new levels of selection emerge, and how  complex genotype-phenotype transitions evolve.</em>
+<h2>
+	Non-random random mutations: Evolution of Genotype-Phenoptype mapping, by <span class="red">Paulien Hogeweg</span>
+</h2>
+
+<p>
+	<strong>Abstract: </strong> 
+
+
+Darwinian evolution, i.e. random mutations and selection, not only leads to
+adaptation to the environment, but more importantly, to the 
+complex multilayered genotype-phenotype mapping, and therewith the mutational neighbourhood.
+
+In evolutionary models with enough degrees of freedom, information integration over evolutionary time will shape the mutational neighbourhood sothat (the effect of) random mutations is not random but biased to be
+ beneficial.
+
+I will show that the structuring of the mutational neighbourbood,
+ may serve as an alternative to  more well known processes like regulation and speciation to cope with temporal and spatial heterogeneous environments. 
+</p>
+
+<div>
+
+	<img src="img/paulien.jpg" width="200px" align="left" style="margin:10px"/>
+
+<p>
+<strong>Bio:</strong><em> Paulien Hogeweg is a Dutch theoretical biologist and complex systems researcher studying biological systems as dynamic information processing systems at many interconnected levels.  Together with Ben Hesper he coined the term "bioinformatics" in the 70's, as the study of informatics processes in biotic systems, including both data analysis and modelling.  Currently her main interest is studying "multilevel evolution": how new levels of selection emerge, and how  complex genotype-phenotype transitions evolve.</em>
+</p>
+</div>
 
        <?php include('footer.php') ?>
