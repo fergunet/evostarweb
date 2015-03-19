c040ee206d5ea6a6e175ae41df19966498fa0092
fergunet <fergunet@gmail.com>
2015-03-19 16:14:57 +0100

    Added references to springerlink proceedings and programme_research_projects

---
+++ proceedings.php
@@ -0,0 +1,14 @@
+<?php include('header.php') ?>
+
+        <h2>Download <span class="red">Proceedings</span><h2>
+   </div>
+
+Temporary free access to the online version of the EvoStar 2015 conference proceedings on SpringerLink are available for registered conference participants of EvoStar 2015 from <strong>1 April to 1 May 2015</strong>.
+<ul>
+<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16501-1">Euro<span class="red">GP</span> 2015, LNCS 9025</a></li>
+<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16468-7">Evo<span class="red">COP</span> 2015, LNCS 9026</a></li>
+<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16498-4">Evo<span class="red">MUSART</span> 2015, LNCS 9027</a></li>
+<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16549-3">Evo<span class="red">Applications</span> 2015, LNCS 9028</a></li>
+</ul>
+          <?php include('footer.php') ?>
+
---
+++ programme_research_projects.php
@@ -0,0 +1,14 @@
+<?php include('header.php') ?>
+
+        <h2>Research Project Promotion<h2>
+   </div>
+
+
+<p>Evo<span color="red">Star</span> welcomes displays of research projects, especially pan-European ones!</p>
+
+<p>If you are involved in a research project relevant to bio-inspired computing, and you would like a chance to show off your project results during the EvoStar poster session, then please contact <a href="mailto:j.willies@napier.ac.uk">the EvoStar coordinator</a> no later than 5 March 2015.</p>
+
+<p>
+Most funded research has a dissemination requirement, and EvoStar can provide a useful audience for your project promotion.     In addition to a poster display board for Wednesday evening 8 April, we offer a free project listing with 250 word description in the conference handbook (copy deadline is strictly 9 March).  This promotional opportunity is offered without cost but is open only to registered EvoStar delegates.  
+</p>
+       <?php include('footer.php') ?>
