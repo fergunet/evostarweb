eb385c2d216f9331e7ef1bf154971bd4d829dce9
Pablo García Sánchez <fergunet@gmail.com>
2015-04-21 16:14:28 +0200

    Updated winners list

--- awards_outstanding.php
+++ awards_outstanding.php
@@ -7,4 +7,75 @@
 
 <img src="img/outstanding1.jpg" class="img-responsive"/><br/><br/>
 <img src="img/outstanding2.jpg" class="img-responsive"/>
+
+<h3>Previous awarded:</h3>
+2006
+Jennifer Willies
+<p>
+
+2007
+<A href="http://www.cs.mun.ca/~banzhaf/">Wolfgang Banzhaf</a>
+
+and
+<A href="http://dces.essex.ac.uk/staff/rpoli/">Riccardo Poli</a>
+
+<p>
+
+<a name="2008"></a>
+<!--url broken 2014 a href="http://web.mac.com/evostar/Site_2/EvoSTAR_Award.html"!-->
+2008
+<A href="http://www.cs.ucl.ac.uk/staff/W.Langdon/">Bill Langdon</a>
+
+and
+<A href="http://www.lri.fr/~marc/">Marc Schoenauer</a>
+
+<br>
+<a href="http://www.flickr.com/photos/nicmcphee/2378963862/">pic</a>
+
+<p>
+2009
+<A href="http://ernesto.dei.uc.pt">Ernesto Costa</a>
+
+and
+<A href="http://www.ce.unipr.it/people/cagnoni/">Stefano Cagnoni</a>
+
+<br>
+<a href="http://www.flickr.com/photos/nicmcphee/2378913548/">pic</a>
+<a href="http://www.flickr.com/photos/bekas/3483364499/">pic</a>
+
+<p>
+2010
+<A href="http://www.hec.unil.ch/people/mtomassini">Marco Tomassini</a>
+
+<br>
+<a href="http://www.flickr.com/photos/61761673@N00/4510927716/">pic</a>
+
+<p>
+2011
+<A href="http://www.elec.york.ac.uk/staff/academic/jfm.html">Julian Miller</a>
+
+
+<p>
+2012
+<A href="http://www.ads.tuwien.ac.at/people/Raidl.html">Guenther R. Raidl</a>
+
+
+<p>
+2013
+<A href="http://people.csail.mit.edu/unamay/">Una-May O'Reilly</a>
+
+and
+<A href="http://www.cs.ru.nl/~elenam/">Elena Marchiori</a>
+
+
+<p>
+2014
+Terence C. Fogarty
+<br>
+<a href="http://www0.cs.ucl.ac.uk/staff/ucacbbl/egp2014/terry_fogarty.jpg">pic</a>
+<a href="https://plus.google.com/u/0/+JJMerelo/posts/bYnqVYfR7ZN?cfem=1">movie</a>
+
+<p>
+
+<em>List also updated in <a href="http://www0.cs.ucl.ac.uk/staff/ucacbbl/egp2011/evostar_award.html">Dr. W. B. Langdon's page.</a></em>
 <?php include('footer.php') ?>
