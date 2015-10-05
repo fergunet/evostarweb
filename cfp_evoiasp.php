e13639a3abb2704ba2d92f0af6e8103c2af2bcad
Pablo García Sánchez <fergunet@gmail.com>
2015-10-05 19:19:42 +0200

    Modified IASP

--- cfp_evoiasp.php
+++ cfp_evoiasp.php
@@ -17,7 +17,7 @@ and Pattern Recognition</h2>
 
 <p>
 Evolutionary algorithms have been shown to be tools which can be used
-effectively in the development of systems (software or hardware) for image \
+effectively in the development of systems (software or hardware) for image 
 analysis, signal processing and pattern recognition in complex domains of
 high industrial and social relevance.
 </p>
@@ -42,43 +42,51 @@ Topics of interest include, but are not limited to:
 <li>applications of evolutionary computation to real-world image analysis</li>
 <li>signal processing and pattern recognition problems</li>
 <li>evolvable vision and signal processing hardware</li>
-<li>hybrid approaches to machine vision, signal processing and</li>
-<li>pattern recognition including evolutionary components</li>
+<li>hybrid approaches to machine vision, signal processing and pattern recognition including evolutionary components</li>
 <li>theoretical developments</li>
 <li>comparisons between different evolutionary techniques and between evolutionary and non-evolutionary techniques in image analysis, signal processing and pattern recognition applications</li>
 <li>time series analysis by means of EC techniques</li>
 </ul>
 
+<h3>Special issue of Evolutionary Intelligence (Springer) on
+"Evolutionary Image Analysis, Signal Processing and Pattern Recognition"</h2>
+
+This year, the authors of the best papers accepted by EvoIASP will be
+invited to submit an extended version of their paper, whose positive
+reviews by the conference PC will be forwarded to the journal reviewers,
+to an upcoming special issue of "Evolutionary Intelligence" (Springer,
+<a href="http://link.springer.com/journal/12065">http://link.springer.com/journal/12065</a>) on "Evolutionary Image Analysis,
+Signal Processing and Pattern Recognition".
+
 <h3>Programme Committee</h3>
-TBA
-<!--
+
 <ul>
-<li>Lucia Ballerini (University of Dundee, UK)</li>
-<li>Vitoantonio Bevilacqua (Politecnico di Bari, Italy)</li>
-<li>Leonardo Bocchi (University of Florence, Italy)</li>
-<li>Sergio Damas (European Center for Soft Computing, Spain)</li>
-<li>Fabio Daolio (Shinshu University, Japan)</li>
-<li>Ivanoe De Falco (ICAR - CNR, Italy)</li>
-<li>Antonio Della Cioppa (University of Salerno, Italy)</li>
-<li>Laura Dipietro (Cambridge, USA)</li>
-<li>Marc Ebner (Ernst Moritz Arndt University, Greifswald, Germany)</li>
-<li>Francesco Fontanella (University of Cassino, Italy)</li>
-<li>Mario Koeppen (Kyushu Institute of Technology, Japan)</li>
-<li>Josep Llados (Universitat Autonoma de Barcelona, Spain)</li>
-<li>Jean Louchet (INRIA, France)</li>
-<li>Evelyne Lutton (INRIA, France)</li>
-<li>Simone Marinai (University of Florence, Italy)</li>
-<li>Pablo Mesejo Santiago (INRIA, France)</li>
-<li>Youssef Nashed (Argonne National Laboratory, USA)</li>
-<li>Ferrante Neri (De Monfort University, UK)</li>
-<li>Gustavo Olague (CICESE, Mexico)</li>
-<li>Riccardo Poli (University of Essex, UK)</li>
-<li>Sara Silva (INESC-ID Lisbon, Portugal)</li>
-<li>Stephen Smith (University of York, UK)</li>
-<li>Giovanni Squillero (Politecnico di Torino, Italy)</li>
-<li>Roberto Ugolotti (Henesis srl, Italy)</li>
-<li>Leonardo Vanneschi (Universidade Nova de Lisboa, Portugal)</li>
-</ul>-->
+<li> Lucia Ballerini (University of Dundee, UK)</li>
+<li>    Vitoantonio Bevilacqua (Politecnico di Bari, Italy)</li>
+<li>    Leonardo Bocchi (University of Florence, Italy)</li>
+<li>    Sergio Damas (European Center for Soft Computing, Spain)</li>
+<li>    Fabio Daolio (Shinshu University, Japan)</li>
+<li>    Ivanoe De Falco (ICAR - CNR, Italy)</li>
+<li>    Antonio Della Cioppa (University of Salerno, Italy)</li>
+<li>    Laura Dipietro (Cambridge, USA)</li>
+<li>    Marc Ebner (Ernst Moritz Arndt University, Greifswald, Germany)</li>
+<li>    Francesco Fontanella (University of Cassino, Italy)</li>
+<li>    Mario Koeppen (Kyushu Institute of Technology, Japan)</li>
+<li>    Jean Louchet (INRIA, France)</li>
+<li>    Evelyne Lutton (INRIA, France)</li>
+<li>    Pablo Mesejo Santiago (INRIA, France)</li>
+<li>    Youssef Nashed (Argonne National Laboratory, USA)</li>
+<li>    Ferrante Neri (De Monfort University, UK)</li>
+<li>    Gustavo Olague (CICESE, Mexico)</li>
+<li>    Riccardo Poli (University of Essex, UK)</li>
+<li>    Sara Silva (INESC-ID Lisbon, Portugal)</li>
+<li>    Stephen Smith (University of York, UK)</li>
+<li>    Andy Song (RMIT, Australia)</li>
+<li>    Giovanni Squillero (Politecnico di Torino, Italy)</li>
+<li>    Roberto Ugolotti (Henesis srl, Italy)</li>
+<li>    Leonardo Vanneschi (Universidade Nova de Lisboa, Portugal)</li>
+<li>    Bing Xue (Victoria University of Wellington, New Zealand)</li>
+</ul>
 
 <h3>Evo<span class="red">IASP</span> track chairs</h3>
 <ul>
