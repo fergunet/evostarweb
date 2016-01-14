51c93517fc50f6b079d222397469874b9bd60e82
Pablo García Sánchez <fergunet@gmail.com>
2016-01-14 20:44:51 +0100

    Modified bursaries and letter

--- participants_bursaries.php
+++ participants_bursaries.php
@@ -1,29 +1,84 @@
 <?php include('header.php') ?>
 
-        <h2>Travel and accommodation bursaries for students<h2>
+        <h2>Travel and accommodation bursaries for students</h2>
    </div>
-   TBA
-<!--
-<h3>Application deadline:  25 February 2015</h3>
+   
 
-<p> 
-A limited number of travel bursaries &amp; pre-paid accommodation places will be available for those students whose papers have been accepted for EuroGP, EvoCOP, EvoMusArt and EvoApplications 2015.  Our intention is to offer assistance to as many students as possible and <strong>applications will only be accepted from those who have already registered for the conferences/workshops</strong>.  Do not apply for an EvoStar bursary until you have completed the registration process.  Priority will be given to post-graduate students whose EvoStar papers have been accepted, especially those travelling transcontinental journeys or from countries outside Europe facing real financial hardship. Providing part-support for conference travel or accommodation costs is meant to make it easier for individuals to obtain additional funding from their own institution, or from other sources.
- </p>
+<h3>Application deadline:  20 February 2016</h3>
+<p>
+A limited number of travel bursaries & pre-paid accommodation places will be available
 
- <p>
-There are two types of financial support:  for <strong>travel</strong> and for <strong>accommodation</strong>.
- </p>
- <p>
-<strong>Travel bursaries</strong> are principally for students travelling inter-continental distances and are limited to a maximum 500 Euro, but the amount awarded may be less.  They are claimed retrospectively and original travel tickets/boarding passes, hotel or similar receipts must be submitted after the conferences for reimbursement.
- </p><p>
-<strong>Accommodation bursaries</strong> are for pre-paid bed and breakfast accommodation for three nights and are primarily aimed at students residing within Europe.  Hotel rooms near the conference venue in Copenhagen have been reserved and costs will be paid directly by the organisers.   Individuals prepared to share in twin accommodation rooms are given high priority, but some single rooms will also be available.  You can specify with whom you would like to share, and you will not be asked to share with anyone who is not of similar gender, age and nationality.   Please note that several strong EvoStar friendships and even research collaborations have resulted from students sharing accommodation!
- </p><p>
-We are trying to support as many EvoStar participants as possible with limited funds so please be realistic in your expectations of the support that can be offered.
- </p><p>
-If you wish to apply for travel bursary or an accommodation place, please complete asap the <a href="https://www.surveymonkey.com/s/EvoStarBursary">BURSARY APPLICATION FORM</a>.   If you have any queries, please email the <a href="mailto:j.willies@napier.ac.uk">EvoStar Conference Coordinator</a>  (including TRAVEL BURSARY QUERY in your email subject header).
- </p><p>
-<strong>The deadline for bursary applications is 25 February 2015</strong> and decisions will be made by 5 March and sent to all applicants.
+to those whose papers have been accepted for EuroGP, EvoCOP, EvoMusArt and 
+
+EvoApplications 2016.  We aim to offer assistance to as many students as possible and 
+
+<strong>applications will only be accepted from those who have already registered for the 
+
+EvoStar conferences</strong>.  Do not apply for an EvoStar bursary until you have completed 
+
+the registration process, otherwise your application will be automatically rejected.  
+
+Providing support for conference travel or accommodation costs is meant to make it 
+
+easier for individuals to obtain additional funding from their own institution, or from other 
+
+sources.
 </p>
--->
+
+<p>There are two types of financial support:  for <strong>travel</strong> and for <strong>accommodation</strong></p>
+
+<p><strong>Travel bursaries</strong> are principally for students travelling inter-continental distances from 
+
+outside Europe.  Claims are made after the conference and require original travel 
+
+tickets/boarding passes, hotel or similar receipts to be submitted with a claim form for 
+
+reimbursement in April. </p>
+
+<p><strong>Accommodation bursaries</strong> are for pre-paid accommodation for three nights and are 
+
+primarily aimed at students residing within Europe.  Twin accommodation within the 
+
+conference venue (Seminário de Vilar) has been reserved and costs will be paid directly 
+
+by the organisers.   Applications from those prepared to share in twin accommodation 
+
+rooms are given the highest priority, but some single rooms will also be available.  You 
+
+can specify with whom you would like to share, and you will not be asked to share with 
+
+anyone who is not of similar gender or nationality.   Please note that several strong 
+
+EvoStar friendships, and even research collaborations, have resulted from students 
+
+meeting and sharing accommodation rooms at EvoStar conferences!
+</p>
+
+<p>
+We are trying to support as many EvoStar participants as possible with limited funds so 
+
+please be realistic in your expectations of the support that can be offered.
+</p>
+
+<p>
+
+If you wish to apply for travel bursary for long distance travel or an accommodation 
+
+place if you are based in Europe, please complete asap the <a href="https://www.surveymonkey.co.uk/r/evostar16bursary">BURSARY APPLICATION 
+
+FORM</a>, and not later than 20 February when final decisions will be taken.   If you have 
+
+any queries, please email the <strong>EvoStar Conference Coordinator</strong> ( j.willies@napier.ac.uk ) (and include TRAVEL 
+
+BURSARY QUERY in your email subject header).</p>
+
+<p>
+The deadline for bursary applications is 20 February 2016 and decisions will be 
+
+notified to all applicants by 23 February.
+</p>
+
+<h2><a href="https://www.surveymonkey.co.uk/r/evostar16bursary">APPLY HERE</a></h2>
+
 
 <?php include('footer.php') ?>
