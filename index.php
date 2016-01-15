b80b09fbfa5c97806ae20dd57a4f97048d8d7f25
Pablo García Sánchez <fergunet@gmail.com>
2016-01-15 18:39:21 +0100

    Added venue

--- index.php
+++ index.php
@@ -18,7 +18,16 @@
 	<p>This year, Evo<span class="red">star</span> conferences will be held in Porto, Portugal, from 30 March to 1 April 2016. Read more about Evo<span class="red">star</a> <a href="about_evostar.php">here</a>.</p>
 <p>
 
-<?php include('common_to_all.php') ?>
+<h3>Location Venue</h3>
+EvoStar 2016 will be held from 9am on Wednesday 30 March until 2pm on Friday 1 April, with an optional Friday afternoon local excursion.
+ 
+<p><strong> 
+The conference venue is the  Seminário de Vilar
+Rua Arcediago Van Zeller , 50, 4050-621 Porto
+ </strong></p>
+<a href="https://www.google.co.uk/maps/dir//Casa+Diocesana+-+Semin%C3%A1rio+De+Vilar,+R.+Arcediago+Vanzeller+50,+4050-621+Porto/@41.1492113,-8.6269489,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0xd24650fa2a566e9:0xe474920b1e2b15b!2m2!1d-8.6283544!2d41.1500313">Google Map Link</a>
+
+
 <h3>Special Issues</h3>
 
 Authors of selected papers will be invited to submit extended versions of their papers to some JCR journals. More information available in each conference page.
--- location_venue.php
+++ location_venue.php
@@ -6,7 +6,13 @@
 
 
 
-TBA
+EvoStar 2016 will be held from 9am on Wednesday 30 March until 2pm on Friday 1 April, with an optional Friday afternoon local excursion.
+ 
+<p><strong> 
+The conference venue is the  Seminário de Vilar
+Rua Arcediago Van Zeller , 50, 4050-621 Porto
+ </strong></p>
+<a href="https://www.google.co.uk/maps/dir//Casa+Diocesana+-+Semin%C3%A1rio+De+Vilar,+R.+Arcediago+Vanzeller+50,+4050-621+Porto/@41.1492113,-8.6269489,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0xd24650fa2a566e9:0xe474920b1e2b15b!2m2!1d-8.6283544!2d41.1500313">Google Map Link</a>
 
 
 
