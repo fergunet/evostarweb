74f1779e7196203278b6cbc5d5a844d48e62fb29
Pablo García Sánchez <fergunet@gmail.com>
2016-01-18 18:12:55 +0100

    Added information of Porto

--- location_porto.php
+++ location_porto.php
@@ -5,5 +5,157 @@
 
    <h3>How To Get To Porto:</h3>
 
-TBA
+Most people attending EvoStar 2016 will probably either fly to Porto, or travel to Lisbon 
+
+and travel the 300 km or the 2.5 to 3 hour train journey northwards to Porto.
+
+<h3>AIR TRAVEL TO PORTO</h3>
+
+Francisco Sa Carneiro Airport is located on the north outskirts of Porto, about 11 kilometers from 
+
+the center of the city. It is serviced by some 20 airline companies which operate both business 
+
+class and low cost flights. Airlines that fly into Porto include
+
+<ul>
+<li>TAP Portugal</li>
+<li>Iberia</li>
+<li>Lufthansa</li>
+<li>Alitalia</li>
+<li>British Airways</li>
+<li>Brussels Airlines</li>
+<li>Czech Airlines</li>
+<li>Swiss</li>
+<li>Ryanair</li>
+<li>EasyJet</li>
+<li>Vueling</li>
+<li>Air Transat</li>
+<li>Transavia</li>
+<li>Luxair</li>
+</ul>
+
+<p>
+Cities with direct and connecting flights to Porto are listed at <a href="http://www.ana.pt/en-US/Aeroportos/porto/Porto/Departures/DestinationsAndAirlines/Pages/DestinationsAndAirlines.aspx">http://www.ana.pt/en-US/Aeroportos/porto/Porto/Departures/DestinationsAndAirlines/Pages/DestinationsAndAirlines.aspx</a>
+
+The airport transfer means are highly reliable, Francisco Sa Carneiro Airport being accessible by 
+
+both public means of transport (buses and metros) and private means (rented cars and taxis).
+</p>
+<p>
+<strong>Getting from the Airport</strong><br/>
+</p>
+<p>
+The Metro Line E (the Violet line) leaves from the airport every 20-30 minutes from 
+
+0600-0100 and the journey to the city centre takes about 25 minutes.
+</p>
+<p>
+See <a href="http://metrodoporto.pt">http://metrodoporto.pt</a> for more information including a metro map.
+</p>
+The Metro station is next to the airport, and you can buy tickets from a machine or the 
+
+ticket office.  A single ticket costs €2.45 and there are also options for a 24 hour or 72 
+
+hour card for unlimited trips valid on the Porto transport network.  This is done on a re-
+
+chargable Andante ticket (keep your ticket to recharge for other journeys). Payment can 
+
+be made in cash (notes/coins) and credit/debit cards. 
+</p>
+<p>
+REMEMBER TO VALIDATE YOUR TICKET in the yellow machines before starting your 
+
+journey, to avoid a hefty fine!
+</p>
+City buses into town  - depending on where you are staying, these bus lines may be 
+
+useful. 
+<ul>
+<li><a href="http://www.urbanosresende.net/pdf/horario120.pdf">Line 120 (Airport <-> Leça da Palmeira and Matosinhos)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/601/volta/601_volta_aept1.pdf">Line 601 (Airport->City centre, Cordoaria)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/601/ida/601_ida_cord3.pdf">Line 601 (City centre, Cordoaria -> Airport)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/602/volta/602_volta_aept1.pdf">Line 602 (Airport->City centre, Cordoaria)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/602/ida/602_ida_cord1.pdf">Line 602 (City centre, Cordoaria->Airport)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/604/volta/604_volta_aept1.pdf">Line 604 (Airport-> Porto, Hospital de São João)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/604/ida/604_ida_hsj3.pdf">Line 604 (Porto, Hospital de São João->Airport)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/3M/volta/3m_volta_aept1.pdf">Night line 3M (Airport->City centre, Avenida dos Aliados)</a></li>
+<li><a href="http://www.stcp.pt/fotos/horarios/3M/ida/3m_ida_aal2.pdf">Night line 3M (City Centre, Avenida dos Aliados->Airport)</a></li> 
+<li><a href="http://www.urbanosresende.net/pdf/horario105N.pdf">Night line 105N (Airport <-> Leça da Palmeira, Matosinhos and Norte Shopping)</a></li> 
+</ul>
+
+<h3>Lisbon-Porto BY TRAIN</h3>
+<p>
+Rail travel from Lisbon to Porto is the suggested travel mode as the prices of buses and 
+
+hire car/road tolls is about the same as the train, and the journey is scenic and relaxed.  
+
+Two types of train are available:  the Intercity (Intercidades) and the Alfa Pendular, which 
+
+is 30 minutes faster with more comfortable seats and free WIFI.  Trains leave regularly 
+
+throughout the day and Information on timetables and prices are at <a href="http://www.cp.pt">www.CP.pt</a>
+</p>
+
+<p>
+Current indicative prices for first and second class tickets in each direction are : Alfa 
+
+Pendular is €42.40/€30.30 and the Intercidades costs €35.90/€24.30 
+</p>
+<p>
+
+Both types of train leave from the Lisboa Santa Apolonia and Oriente stations. 
+</p>
+<p>
+
+The arrival station in Porto is at Campanhã station, to the east of the centre, but you can 
+
+either take the metro or with your Lisboa-Porto ticket, you are allowed to transfer to a 
+
+suburban train free of charge for the five minute journey to São Bento station in the city 
+
+centre. Taxis are also available as well as local buses.
+</p>
+
+<h3>BY CAR</h3>
+
+Porto is literally surrounded by motorways, meaning the city enjoys excellent road connections with 
+
+the entire Portugal. Thus, for instance, A1 links Porto to Lisbon, A4 provides the connection to 
+
+Trás-os-Montes, whereas A3 reaches all the way to Minho. Braga, Aveiro, Viana do Castelo are 
+
+also connected by motorways to Porto, so, depending on the chosen destination, you must opt for 
+
+either A29 or A28 in order to get to where they want to reach. Either by bus or by car (private 
+
+vehicle or rented car), Porto can be reached from virtually all corners of the country. Estradas de 
+
+Portugal offers complete information on the motorway and national road networks of Portugal, 
+
+which is why foreigners who want to get to Porto are referred to their <a href="http://www.estradasdeportugal.pt/">official website.</a>
+
+<h3>BY BUS</h3>
+<p>
+Getting to Porto by bus is yet another inspired alternative for people who travel from abroad and 
+
+for nationals alike. Bus trips have their inner advantages, and they can be taken into account by 
+
+people who, for one reason or another, rule out flights and train trips, eventually opting for this 
+
+manner of transport.</p>
+<p>
+The Lisbon headquartered Rede Nacional de Expressos links Porto to virtually the entire Portugal. 
+
+One of the most frequented routes is the one between Porto and Lisbon. Thus, the company 
+
+operates 9 trips a day between the capital and Porto, which is fairly reassuring for all tourists 
+
+(business and pleasure tourists alike) who need to get from one destination to another in a 
+
+relatively short time.
+</p>
+
+For complete and updated information on the bus trips, arrivals and departures, network of lines, 
+
+as well as ticket prices and the like, see <a href="http://www.rede-expressos.pt/default.aspx">Rede Nacional de Expressos</a>.
     <?php include('footer.php') ?>
--- location_venue.php
+++ location_venue.php
@@ -11,9 +11,12 @@ EvoStar 2016 will be held from 9am on Wednesday 30 March until 2pm on Friday 1 A
 <p><strong> 
 The conference venue is the  Seminário de Vilar
 Rua Arcediago Van Zeller , 50, 4050-621 Porto
- </strong></p>
+ </strong>
+which is located close to the historical centre of O’Porto.
+
+The closest Metro stations are “Carolina Michaelis” “Casa da Música”.
+</p>
 <a href="https://www.google.co.uk/maps/dir//Casa+Diocesana+-+Semin%C3%A1rio+De+Vilar,+R.+Arcediago+Vanzeller+50,+4050-621+Porto/@41.1492113,-8.6269489,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0xd24650fa2a566e9:0xe474920b1e2b15b!2m2!1d-8.6283544!2d41.1500313">Google Map Link</a>
-
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d12017.308767501161!2d-8.6269489!3d41.1492113!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0xd24650fa2a566e9%3A0xe474920b1e2b15b!2sCasa+Diocesana+-+Semin%C3%A1rio+De+Vilar%2C+R.+Arcediago+Vanzeller+50%2C+4050-621+Porto!3m2!1d41.150031299999995!2d-8.6283544!5e0!3m2!1ses!2suk!4v1453134311974" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 
     <?php include('footer.php') ?>
