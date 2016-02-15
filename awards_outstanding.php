88215656df4fe3c8f75d34268609f512f9ec5a4f
Pablo García Sánchez <fergunet@gmail.com>
2016-02-15 14:10:51 +0100

    Outstanding updated

--- awards_outstanding.php
+++ awards_outstanding.php
@@ -3,82 +3,34 @@
 <h2>Evo<span class="red">Star</span> Award for Outstanding Contribution to Evolutionary Computation in Europe 2016 </h2>
 </div>
 
-To be decided
-<!--
-<h2>Winners: <a href="https://sites.google.com/site/aiesparcia/">Anna Esparcia-Alcazar</a> and <a href="http://www.novaims.unl.pt/Docentes-Investigacao/Detalhe-Docente.asp?IDDocente=196">Leonardo Vanneschi</a></h2>
 
-<img src="img/outstanding1.jpg" class="img-responsive"/><br/><br/>
-<img src="img/outstanding2.jpg" class="img-responsive"/>
+
+<h2>Winners: </h2>
+
+To be decided
 
 <h3>Previous awarded:</h3>
-2006
-Jennifer Willies
-<p>
-
-2007
-<A href="http://www.cs.mun.ca/~banzhaf/">Wolfgang Banzhaf</a>
-
-and
-<A href="http://dces.essex.ac.uk/staff/rpoli/">Riccardo Poli</a>
-
-<p>
-
-<a name="2008"></a>
-2008
-<A href="http://www.cs.ucl.ac.uk/staff/W.Langdon/">Bill Langdon</a>
-
-and
-<A href="http://www.lri.fr/~marc/">Marc Schoenauer</a>
-
-<br>
-<a href="http://www.flickr.com/photos/nicmcphee/2378963862/">pic</a>
-
-<p>
-2009
-<A href="http://ernesto.dei.uc.pt">Ernesto Costa</a>
-
-and
-<A href="http://www.ce.unipr.it/people/cagnoni/">Stefano Cagnoni</a>
-
-<br>
-<a href="http://www.flickr.com/photos/nicmcphee/2378913548/">pic</a>
-<a href="http://www.flickr.com/photos/bekas/3483364499/">pic</a>
-
-<p>
-2010
-<A href="http://www.hec.unil.ch/people/mtomassini">Marco Tomassini</a>
-
-<br>
-<a href="http://www.flickr.com/photos/61761673@N00/4510927716/">pic</a>
-
-<p>
-2011
-<A href="http://www.elec.york.ac.uk/staff/academic/jfm.html">Julian Miller</a>
 
 
-<p>
-2012
-<A href="http://www.ads.tuwien.ac.at/people/Raidl.html">Guenther R. Raidl</a>
 
 
-<p>
-2013
-<A href="http://people.csail.mit.edu/unamay/">Una-May O'Reilly</a>
+<ul>
+<li>2015: <a href="https://sites.google.com/site/aiesparcia/">Anna Esparcia-Alcazar</a> and <a href="http://www.novaims.unl.pt/Docentes-Investigacao/Detalhe-Docente.asp?IDDocente=196">Leonardo Vanneschi</a> <a href="2015/awards_outstanding.php">[announcement page]</a></li>
+<li>2014: Terence C. Fogarty <a href="http://www0.cs.ucl.ac.uk/staff/ucacbbl/egp2014/terry_fogarty.jpg">[pic]</a> <a href="https://plus.google.com/u/0/+JJMerelo/posts/bYnqVYfR7ZN?cfem=1">[movie]</a></li>
+<li>2013: <A href="http://people.csail.mit.edu/unamay/">Una-May O'Reilly</a> and <A href="http://www.cs.ru.nl/~elenam/">Elena Marchiori</a></li>
+<li>2012: <A href="http://www.ads.tuwien.ac.at/people/Raidl.html">Guenther R. Raidl</a></li>
+<li>2011: <A href="http://www.elec.york.ac.uk/staff/academic/jfm.html">Julian Miller</a></li>
+<li>2010: <A href="http://www.hec.unil.ch/people/mtomassini">Marco Tomassini</a> <a href="http://www.flickr.com/photos/61761673@N00/4510927716/">[pic]</a></li>
+<li>2009: <A href="http://ernesto.dei.uc.pt">Ernesto Costa</a> and <A href="http://www.ce.unipr.it/people/cagnoni/">Stefano Cagnoni</a> <a href="http://www.flickr.com/photos/nicmcphee/2378913548/">[pic]</a> <a href="http://www.flickr.com/photos/bekas/3483364499/">[pic]</a> </li>
+<li>2008: <A href="http://www.cs.ucl.ac.uk/staff/W.Langdon/">Bill Langdon</a> and <A href="http://www.lri.fr/~marc/">Marc Schoenauer</a> <a href="http://www.flickr.com/photos/nicmcphee/2378963862/">[pic]</a></li>
+<li>2007: <A href="http://www.cs.mun.ca/~banzhaf/">Wolfgang Banzhaf</a> and <A href="http://dces.essex.ac.uk/staff/rpoli/">Riccardo Poli</a></li>
+<li>2006: Jennifer Willies</li>
+</ul>
 
-and
-<A href="http://www.cs.ru.nl/~elenam/">Elena Marchiori</a>
 
 
-<p>
-2014
-Terence C. Fogarty
-<br>
-<a href="http://www0.cs.ucl.ac.uk/staff/ucacbbl/egp2014/terry_fogarty.jpg">pic</a>
-<a href="https://plus.google.com/u/0/+JJMerelo/posts/bYnqVYfR7ZN?cfem=1">movie</a>
-
-<p>
 
 <em>List also updated in <a href="http://www0.cs.ucl.ac.uk/staff/ucacbbl/egp2011/evostar_award.html">Dr. W. B. Langdon's page.</a></em>
--->
+
 
 <?php include('footer.php') ?>
