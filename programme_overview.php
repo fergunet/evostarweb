c3c8a77357bc769823621677692b4aa7108d64cf
Pablo García Sánchez <fergunet@gmail.com>
2016-03-15 13:30:07 +0100

    Changed pdf names and overview programme

--- programme_overview.php
+++ programme_overview.php
@@ -1,12 +1,12 @@
 
 <?php include('header.php') ?>
 
-        <h3>Evostar <span class="red">Overview Programme</span> <h3>
+        <h3>Evostar <span class="red">Programme</span> <h3>
    </div>
-
-<a href="pdfs/EuroGPprogramme.pdf"><h3>EuroGP</h3></a>
-<a href="pdfs/EvoMUSARTprogramme.pdf"><h3>EvoMUSART</h3></a>
-<a href="pdfs/EvoCOPprogramme.pdf"><h3>EvoCOP</h3></a>
-<a href="#"><h3>EvoAPPS (TBA)</h3></a>
+<a href="pdfs/OverviewProgramme.pdf"><h3>Sessions schedule</h3></a>
+<h3>Euro<span class="red">GP</span>: Full programme (TBA). <a href="pdfs/EuroGPschedule.pdf">[Schedule]</a></h3></a>
+<h3>Evo<span class="red">MUSART</span>: Full programme (TBA). <a href="pdfs/EvoMUSARTschedule.pdf">[Schedule]</a></h3></a>
+<h3>Evo<span class="red">COP</span>: Full programme (TBA). <a href="pdfs/EvoCOPschedule.pdf">[Schedule]</a></h3></a>
+<h3>Evo<span class="red">APPS</span>: <a href="pdfs/EvoAPPSFullProgramme.pdf">Full programme</a>. <a href="pdfs/EvoAPPSschedule.pdf">[Schedule]</a></h3></a>
 
 <?php include('footer.php') ?>
