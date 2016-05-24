444649a094b15ac8f4954049c88ad33192701495
Pablo García Sánchez <fergunet@gmail.com>
2016-05-24 17:01:02 +0200

    Updating organisers

--- about_organisers.php
+++ about_organisers.php
@@ -1,24 +1,25 @@
 <?php include('header.php') ?>
 
-        <h2>Evo<span class="red">Star</span> 2016 Organizers<h2>
+        <h2>Evo<span class="red">Star</span> 2017 Organizers<h2>
    </div>
 
 
 <h3>Euro<span class="red">GP</span></h3>
-<h4>19th European Conference on Genetic Programming</h4>
+<h4>20th European Conference on Genetic Programming</h4>
  
 <strong>Euro<span class="red">GP</span> programme chairs :</strong>
 
 <table class="organisers">
 <tr>
 	<!--<td><img src="organisers/penousal_machado.jpg" width="100"></td>-->
-	<td><img src="organisers/malcolm_heywood.jpg" width="100"></td>
+
 	<td><img src="organisers/james_mcdermott.jpg" width="100"></td>
+	<td><img src="organisers/mauro_castelli.jpg" width="100"></td>
 </tr>
 <tr>
 	<!--<td>Penousal Machado, University of Coimbra, Portugal, machado(at)dei.uc.pt</td>-->
-	<td>Malcolm Heywood, Dalhousie University, Canada, mheywood(at)cs.dal.ca</td>
-	<td>James McDermott,  University College Dublin, Ireland, jamesmichaelmcdermott(at)gmail.com</td>
+	<td>James McDermott,  University College Dublin, Ireland,jmmcd(at)jmmcd.net</td>
+	<td>Mauro Castelli, NOVA IMS, Universidade Nova de Lisboa, Portugal, mcastelli(at)novaims.unl.pt</td>
 	
 </tr>
 </table>
@@ -28,10 +29,10 @@
 <strong>Euro<span class="red">GP</span> publication chair :</strong>
 <table class="organisers">
 <tr>
-	<td><img src="organisers/mauro_castelli.jpg" width="100"></td>
+	<td><img src="organisers/lukas_sekanina.jpg" width="100"></td>
 </tr>
 <tr>
-	<td>Mauro Castelli, NOVA IMS, Universidade Nova de Lisboa, Portugal, castelli.mauro(at)gmail.com</td>
+	<td>Lukáš Sekanina, Brno University of Technology, Czech Republic, sekanina(at)fit.vutbr.cz</td>
 </tr>
 </table>
 
@@ -39,18 +40,18 @@
  
  
 <h3>Evo<span class="red">COP</span></h3>
-<h4>16th European Conference on Evolutionary Computation in Combinatorial Optimisation</h4>
+<h4>17th European Conference on Evolutionary Computation in Combinatorial Optimisation</h4>
  
 <strong>Evo<span class="red">COP</span> programme chairs :</strong>
 
 <table class="organisers">
 <tr>
 	<td><img src="organisers/bin_hu.jpg" width="100"></td>
-	<td><img src="organisers/jose_francisco_chicano.jpg" width="100"></td>
+	<td><img src="organisers/manuel_lopez_ibanez.jpg" width="100"></td>
 </tr>
 <tr>
 	<td>Bin Hu, Austrian Institute of Technology, Austria, bin.hu(at)ait.ac.at</td>
-	<td>Francisco Chicano, University of Malaga, Spain, chicano(at)lcc.uma.es</td>
+	<td>Manuel López-Ibáñez, University of Manchester, UK, manuel.lopez-ibanez(at)manchester.ac.uk)</td>
 </tr>
 </table>
 
@@ -61,17 +62,17 @@
  
  
 <h3>Evo<span class="red">MUSART</span></h3>
-<h4>5th International Conference on Evolutionary and Biologically Inspired Music, Sound, Art and Design</h4>
+<h4>6th International Conference on Evolutionary and Biologically Inspired Music, Sound, Art and Design</h4>
  
 <strong>Evo<span class="red">MUSART</span> programme chairs :</strong>
 
 <table class="organisers">
 <tr>
-	<td><img src="organisers/colin_johnson.png" width="100"></td>
+	<td><img src="organisers/joao_correia.png" width="100"></td>
 	<td><img src="" width="100"></td>
 </tr>
 <tr>
-	<td>Colin Johnson, University of Kent, UK, c.g.johnson(at)kent.ac.uk</td>
+	<td>João Correia, University of Coimbra, Portugal,  jncor(at)dei.uc.pt)</td>
 	<td>Vic Ciesielski, RMIT University, vic.ciesielski(at)rmit.edu.au </td>
 </tr>
 </table>
@@ -82,10 +83,10 @@
 <strong>Evo<span class="red">MUSART</span> Publication chair :</strong>
 <table class="organisers">
 <tr>
-	<td><img src="organisers/joao_correia.jpg" width="100"></td>
+	<td><img src="organisers/" width="100"></td>
 </tr>
 <tr>
-	<td>Jo&atilde;o Correia, University of Coimbra, Portugal, jncor(at)dei.uc.pt</td>
+	<td>??????</td>
 </tr>
 </table>
 
@@ -94,7 +95,7 @@
  
  
 <h3>Evo<span class="red">APPLICATIONS</span></h3>
-<h4>18th European Conference on the Applications of Evolutionary Computation</h4>
+<h4>20th European Conference on the Applications of Evolutionary Computation</h4>
 <strong>Evo<span class="red">Applications</span> coordinator :</strong>
 
 <table class="organisers">
@@ -109,16 +110,16 @@
 <strong>Evo<span class="red">APPS</span> publication chair :</strong>
 <table class="organisers">
 <tr>
-	<td><img src="organisers/paolo_burelli.jpg" width="100"></td>
+	<td><img src="organisers/kevin_sim.jpg" width="100"></td>
 </tr>
 <tr>
-	<td>Paolo Burelli, Aalborg University, Copenhagen, Denmark, pabu(at)create.aau.dk</td>
+	<td>Kevin Sim, Edinburgh Napier University, UK, k.sim(at)napier.ac.uk</td>
 </tr>
 </table>
 
 
  
-<h3>Evo<span class="red">Star</span> 2016 publicity chair</h3>
+<h3>Evo<span class="red">Star</span> 2017 publicity chair</h3>
 <table class="organisers">
 <tr>
 	<td><img src="organisers/pablo_garcia.jpg" width="100"></td>
@@ -129,18 +130,29 @@
 	
 </tr>
 </table>
- 
-<h3>Evo<span class="red">Star</span> 2016 local chair</h3>
+
+<h3>Evo<span class="red">Star</span> 2017 MyReview System coordinator</h3>
 
 <table class="organisers">
 <tr>
 	
-	<td><img src="organisers/penousal_machado.jpg" width="100"></td>
-	<td><img src="organisers/ernesto_costa.jpg" width="100"></td>
+	<td><img src="organisers/marc_scho.jpg" width="100"></td>
 </tr>
 <tr>
-	<td>Penousal Machado, University of Coimbra, Portugal, machado(at)dei.uc.pt</td>
-	<td>Ernesto Costa, University of Coimbra, Portugal, ernesto@dei.uc.pt</td>
+	<td>Marc Schoenauer, INRIA, France, marc.schoenauer(at)inria.fr </td>
+</tr>
+</table>
+
+
+<h3>Evo<span class="red">Star</span> 2017 local chair</h3>
+
+<table class="organisers">
+<tr>
+	
+	<td><img src="organisers/evert_haasdijk.jpg" width="100"></td>
+</tr>
+<tr>
+	<td>Evert Haasdijk, VU University Amsterdam, Netherlands, e.haasdijk(at)vu.nl </td>
 </tr>
 </table>
  
