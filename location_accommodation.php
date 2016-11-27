cbaeea6b93cb17247ee6da5f69293a147929f5a0
Pablo García Sánchez <fergunet@gmail.com>
2016-11-27 14:45:35 +0100

    Modified accommodation

--- location_accommodation.php
+++ location_accommodation.php
@@ -3,5 +3,10 @@
 
         <h2>Accommodation <span class="red">Information</span><h2>
    </div>
-TBA
+This link <a href="https://smartbyreservations.nl/Eventguest/Register?ID=7kKA2Hjo8f4=&key=1">https://smartbyreservations.nl/Eventguest/Register?ID=7kKA2Hjo8f4=&key=1</a>
+allows attendants to book selected hotels at a reduced rate.
+
+<br/>
+More information about other recommended hotels to be added. 
+
  <?php include('footer.php') ?>
