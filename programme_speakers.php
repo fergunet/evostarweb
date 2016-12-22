a8c306ae8df343a9ac01c3702e5dc8c565023448
Pablo García Sánchez <fergunet@gmail.com>
2016-12-22 13:34:12 +0100

    Added speakers

--- programme_speakers.php
+++ programme_speakers.php
@@ -3,49 +3,54 @@
         <h2>Invited Speakers<h2>
    </div>
 
-   TBA
+   
 
-<!-- <h2>
-	THE EVOLUTION OF BEAGLE: CONFESSIONS OF A MONGREL RULE-BREEDER, by <span class="red">Richard Forsyth</span></h2>
+ <h2>
+	EC: Past, Present and Future by <span class="red">Kenneth De Jong</span></h2>
 
 
 <p>
-<strong>Abstract:</strong> This talk falls into three main parts (as recommended by Aristotle). First, a look back at the heady days of the 1980s when evolutionary computing had an appealing air of raffish unorthodoxy at a time of peak Prolog. This will include a biased perspective on the genealogy of Genetic Programming, summarized in a simple spreadsheet as a staircase of synthetic serendipity, which I intend to use to explore the issue of what exactly are the defining features of GP, and who decides. Then I plan to discuss some of the design choices that arose in reviving and revising BEAGLE, in the light of experience with earlier versions. Some of these choices were guided by experimental trials, whose results will be reported. This reporting will include coverage of a couple of apparently neat ideas that don't seem to work. Finally, I will look ahead to the evolution (sorry, hard to resist sometimes) of the field. This provides an opportunity to indulge in some crystal-ball-gazing at indistinct impressions of the cybernetworked dystopia awaiting our grandchildren if the big-data fanatics ever get their acts together -- while still leaving time for questions from the audience.
-</p>
+	<span class="red">Opening Talk on Wednesday 19 April 2017</span>
+</p>	
+
+<p>
+<strong>Abstract:</strong> TBA </p>
 
 <div>
 
-	<img src="img/forsyth.jpg" width="200px" align="right" style="margin:10px"/>
+	<img src="img/dejong.jpg" width="200px" align="right" style="margin:10px"/>
 
 <p>
 <strong>Bio:</strong><em> 
 
 
-Richard Forsyth is a relic from the medieval period of evolutionary computing. In 1981, inspired by even 
+Kenneth A. De Jong received his Ph.D. in computer science from the University of
 
-earlier pioneers like Oliver Selfridge and Gordon Pask, he published an account of what was arguably the 
+Michigan in 1975. He joined George Mason University in 1984 and is currently a Professor
 
-first working example of tree-structured program code optimized by evolutionary methods. Nowadays it 
+Emeritus of Computer Science, head of the <a href="http://www.cs.gmu.edu/~eclab">Evolutionary Computation Laboratory</a>, and
 
-would probably be called Genetic Programming. At the time he called it a Darwinian rule-learner trained by 
+Associate Director of the <a href="http://www.krasnow.gmu.edu/">Krasnow Institute</a>. His research interests include genetic
 
-"naturalistic selection". The term didn't catch on. In 1985 he started selling a PC version of the BEAGLE rule-
+algorithms, evolutionary computation, machine learning, and complex adaptive systems.
 
-finder system (Biological Evolutionary Algorithm Generating Logical Expressions). Fortunately his lack of 
+He is currently involved in research projects involving the development of new evolutionary
 
-business acumen spared him from the fate of becoming a software billionaire. In consequence, he has spent 
+algorithm (EA) theory, the use of EAs as high-performance optimization techniques, and
 
-most of the last 30 years in the lower echelons of Britain's "higher" education system. His peregrinations 
+the application of EAs to the problem of learning task programs in domains such as robot
 
-have taken him on a tour through a motley assortment of faculties, including Arts, Medicine, Science and 
+navigation and game playing. He is an active member of the Evolutionary Computation
 
-Social Sciences. Released from the academic hamster-wheel, he has recently revived & revised BEAGLE, with 
+research community and has been involved in organizing many of the workshops and
 
-a few novel twists that will be described in his talk. This activity has caused BEAGLE to give birth to a rather 
+conferences in this area. He is the founding editor-in- chief of the journal Evolutionary
 
-mischievous pup called RUNSTER (Regression Using Naturalistic Selection To Evolve Rules). Both these 
+Computation (MIT Press), and a member of the board of ACM SIGEVO. He is the
 
-systems will soon be made freely available to all & sundry on Richard's website.
+recipient of an IEEE Pioneer award in the field of Evolutionary Computation and a lifetime
+
+achievement award from the Evolutionary Programming Society.
 
 </p>
 </em>
@@ -54,26 +59,67 @@ systems will soon be made freely available to all & sundry on Richard's website.
 
 
 <h2>
-	Metaphors in metaheuristics - a symptom of a deeper ailment?
+Evolutionary Computation in Industry: A Realistic Overview by <span class="red">Arthur Kordon</span>
 	</h2>
+
+<p> <span class="red">Closing Talk on Friday 21 April 2017</span></p>
 
 <p>
 	<strong>Abstract: </strong> 
+This talk will give a realistic overview of the current state of the art of EC in industry based
 
+on 20 years experience from applying these systems in several large global corporations.
 
-Consensus is forming in (a large part of) the metaheuristics community that there is a problem with the current flood of "novel" metaphor-based methods. In the paper "Metaheuristics - the metaphor exposed", I have investigated why the field of heuristic optimization is especially vulnerable to this kind of bad science. In this talk, I further investigate this issue and arrive at the conclusion that the entire field of heuristic optimization is in need of an update of its standards. </p>
+It includes a short analysis of the differences between academic and industrial research,
+
+examples of the key implementation areas of EC in manufacturing and business, a
+
+discussion about the main factors for success and failure of industrial EC systems, and an
+
+estimate of the projected industrial needs that may drive future applications.
+
 
 <div>
 
-	<img src="img/kenneth.jpg" width="200px" align="left" style="margin:10px"/>
+	<img src="img/Arthur_Kordon.jpg" width="200px" align="left" style="margin:10px"/>
 
 <p>
 <strong>Bio:</strong><em> 
-Kenneth Sörensen (Antwerp, Belgium, 1974) holds a PhD from the University of Antwerp, obtained in 2003. Currently, he is a Research Professor of the Faculty of Applied Economics of the University of Antwerp. Within this Faculty, he founded the ANT/OR research group, that focuses on applications of operations research. Kenneth Sörensen has published a large number of articles in international refereed journals, and has presented his work at numerous scientic conferences. His main research interests are the application of advanced (metaheuristic) optimization methods and the development and study of optimization methods.
 
-Kenneth Sörensen is the founder and current coordinator of the EURO working group EU/ME – the metaheuristics community, the largest online platform for researchers in metaheuristics worldwide. He is also associate editor of the Journal of Heuristics, International Transactions in Operational Research, and 4OR. 
+Dr. Arthur Kordon is CEO of Kordon Consulting LLC and an internationally recognized
+
+expert in applying data science, advanced analytics and computational intelligence in
+
+industry. The list of his current projects for large corporations includes: customer churn
+
+analysis, classifying patterns in office occupancy, nonlinear price forecasting, etc. In his
+
+previous position as Advanced Analytics Leader at Dow Chemical, Dr. Kordon has
+
+successfully applied advanced analytics solutions in various business problems in
+
+forecasting, business cycles analysis, price elasticity analysis, fraud detection in Auditing,
+
+etc. He also successfully introduced several novel technologies for improved
+
+manufacturing and new product design based on computational intelligence, such as
+
+robust inferential sensors, process optimization based on empirical emulators, automated
+
+operating discipline, and accelerated fundamental model building. He holds a US patent
+
+and has published more than 70 papers and 14 book chapters in the area of applied
+
+computational intelligence and advanced analytics. Recently he published the book
+
+Applying Computational Intelligence, published by Springer and is a co-author of the book
+
+Applied Data Mining for Forecasting, published by SAS Press.
+
+
+
 </em>
 </p>
 </div>
--->
+
        <?php include('footer.php') ?>
