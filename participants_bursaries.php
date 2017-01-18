8e4d4b7cf075b5251f16bfdc3f6352f9c8a3a9c1
Pablo García Sánchez <fergunet@gmail.com>
2017-01-18 15:46:04 +0100

    Student bursaries

--- participants_bursaries.php
+++ participants_bursaries.php
@@ -1,87 +1,31 @@
 <?php include('header.php') ?>
 
-        <h2>Travel and accommodation bursaries for students</h2>
+        <h2>Student Travel Bursaries</h2>
    </div>
-   TBA
-   <!--
+   
+   
 
-<h3>Application deadline:  20 February 2016</h3>
 
-<h2><span class="red">NOTE: APPLICATION IS NOW CLOSED</span></h2>
-<p>
-A limited number of travel bursaries & pre-paid accommodation places will be available
 
-to those whose papers have been accepted for EuroGP, EvoCOP, EvoMusArt and 
+<!--h2><span class="red">NOTE: APPLICATION IS NOW CLOSED</span></h2>-->
 
-EvoApplications 2016.  We aim to offer assistance to as many students as possible and 
-
-<strong>applications will only be accepted from those who have already registered for the 
-
-EvoStar conferences</strong>.  Do not apply for an EvoStar bursary until you have completed 
-
-the registration process, otherwise your application will be automatically rejected.  
-
-Providing support for conference travel or accommodation costs is meant to make it 
-
-easier for individuals to obtain additional funding from their own institution, or from other 
-
-sources.
-</p>
-
-<p>There are two types of financial support:  for <strong>travel</strong> and for <strong>accommodation</strong></p>
-
-<p><strong>Travel bursaries</strong> are principally for students travelling inter-continental distances from 
-
-outside Europe.  Claims are made after the conference and require original travel 
-
-tickets/boarding passes, hotel or similar receipts to be submitted with a claim form for 
-
-reimbursement in April. </p>
-
-<p><strong>Accommodation bursaries</strong> are for pre-paid accommodation for three nights and are 
-
-primarily aimed at students residing within Europe.  Twin accommodation within the 
-
-conference venue (Seminário de Vilar) has been reserved and costs will be paid directly 
-
-by the organisers.   Applications from those prepared to share in twin accommodation 
-
-rooms are given the highest priority, but some single rooms will also be available.  You 
-
-can specify with whom you would like to share, and you will not be asked to share with 
-
-anyone who is not of similar gender or nationality.   Please note that several strong 
-
-EvoStar friendships, and even research collaborations, have resulted from students 
-
-meeting and sharing accommodation rooms at EvoStar conferences!
-</p>
+<h3>Application deadline:  20 February 2017</h3>
+ 
+<p>A limited number of student travel bursaries will be available to those whose papers have been accepted for EuroGP, EvoCOP, EvoMUSART and EvoAPPLICATIONS 2017.  We aim to offer assistance to as many students as possible and <span class="red" style="font-weight:bold;">applications will only be accepted from those who have already registered for the EvoStar conferences</span>.  Do not apply for an EvoStar bursary until you have completed the registration process, otherwise your application will be automatically rejected.  
+ </p>
+ <p>
+Providing support for conference travel is meant to make it easier for individuals to obtain additional funding from their own institution, or from other sources.  <strong>Travel bursaries</strong> are principally designed for students travelling inter-continental distances from outside Europe.  Claims are made after the conference and require original travel tickets/boarding passes to be submitted with a claim form, for reimbursement in April. 
+ </p>
+ <p>
+We are trying to support as many EvoStar participants as possible with limited funds so please be realistic in your expectations of the support that can be offered.
+ </p>
+ <p>
+If you wish to apply for travel bursary, please complete asap the <a href="https://www.surveymonkey.co.uk/r/Star17bursary">BURSARY APPLICATION FORM</a>, and not later than 20 February when final decisions will be taken.   If you have any queries, please email the EvoStar Conference Coordinator  (and include TRAVEL BURSARY QUERY in your email subject header).
+ </p>
+<span>The deadline for bursary applications is 20 February 2017 and decisions will be notified to all applicants by 23 February.</span>
 
 <p>
-We are trying to support as many EvoStar participants as possible with limited funds so 
-
-please be realistic in your expectations of the support that can be offered.
+<h3><a href="https://www.surveymonkey.co.uk/r/Star17bursary">APPLY HERE</a></h3>
 </p>
 
-<p>
-
-If you wish to apply for travel bursary for long distance travel or an accommodation 
-
-place if you are based in Europe, please complete asap the <a href="https://www.surveymonkey.co.uk/r/evostar16bursary">BURSARY APPLICATION 
-
-FORM</a>, and not later than 20 February when final decisions will be taken.   If you have 
-
-any queries, please email the <strong>EvoStar Conference Coordinator</strong> ( j.willies@napier.ac.uk ) (and include TRAVEL 
-
-BURSARY QUERY in your email subject header).</p>
-
-<p>
-The deadline for bursary applications is 20 February 2016 and decisions will be 
-
-notified to all applicants by 23 February.
-</p>
-
-<h2><a href="https://www.surveymonkey.co.uk/r/evostar16bursary">APPLY HERE</a></h2>
-
--->
 <?php include('footer.php') ?>
