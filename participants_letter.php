b6e675fbbff96288d032212b9d6c1aa77c7251cd
Pablo García Sánchez <fergunet@gmail.com>
2017-01-19 13:13:00 +0100

    Letter and registration

--- participants_letter.php
+++ participants_letter.php
@@ -2,52 +2,17 @@
 
         <h2>Letters of Invitation to EvoStar<h2>
    </div>
-TBA
-<!--
-<p>Letters of invitation to EvoStar 2016 for the purposes of obtaining a visa to 
-
-enter Portugal are only prepared for authors of accepted papers, or for 
-
-genuine conference researchers working or studying at recognised academic 
-
-institutions active in relevant subjects.  The are issued at the discretion of the 
-
-conference organisers.</p>
 
 
-<p>
-Letters of invitation are only sent when <strong>registration fees have been paid in 
-
-full</strong>.  Applications should be made by <strong>29 February 2016</strong> to allow time for 
-
-processing by consular authorities who may take weeks to issue a visa. 
-
-Please check with your local Portuguese consular office to know the correct 
-
-visa application procedure in your country, as there are local differences.
+ <p>
+Letters of invitation to EvoStar 2017 for the purposes of obtaining a visa to enter the Netherlands are only prepared for authors of accepted papers, or for genuine conference researchers working or studying at recognised academic institutions who are active in relevant subjects.  These invitations are issued at the discretion of the conference organisers.
 </p>
 <p>
-
-
-A letter of invitation can only be obtained by <a href="https://www.surveymonkey.co.uk/r/LetInvite2016">submitting this form</a>.  Each 
-
-author requiring a visa should complete their own form, since individual 
-
-letters of invitation are prepared.  It may take up to 10 working days for an 
-
-invitation letter to be sent following your application.  
+Letters of invitation <span class="red">are only sent when registration fees have been paid in full</span>.  Applications should be made by <span class="red">28 February 2017</span> to allow time for processing by consular authorities who may take weeks to issue a visa. Please check with your local Netherlands consular office to know the correct visa application procedure in your country, as there are local differences.
+ </p>
+<p></p>
+A letter of invitation can only be obtained by <a href="https://www.surveymonkey.com/r/Star17invitation">submitting this form</a>.  Each author requiring a visa should complete their own form, since individual letters of invitation are prepared.  It may take up to 10 working days for an invitation letter to be sent following your application. 
+ <p>
+Do not contact the conference organisers unless you have a particular enquiry, otherwise you will simply be re-directed to the form.  Genuine questions for clarification purposes can be sent to the EvoStar Coordinator, <a href="mailto:jenniferwillies@gmail.com">Jennifer Willies</a> ( jenniferwillies@gmail.com ) .    Please include “EvoStar Letter of Invitation” in the subject header of your email.
 </p>
-<p>
-
-Do not contact the conference organisers unless you have a particular 
-
-enquiry, otherwise you will simply be re-directed to the form.  Genuine 
-
-questions for clarification purposes can be sent to the EvoStar Coordinator, 
-
-<strong>Jennifer Willies</strong> ( j.willies@napier.ac.uk ).    Please include “EvoStar Letter of Invitation” in the subject 
-
-header of your email.
-</p>
--->
 <?php include('footer.php') ?>
--- participants_registration.php
+++ participants_registration.php
@@ -2,97 +2,30 @@
 
         <h2>Evo* 2017 REGISTRATION<h2>
    </div>
-TBA
-<!--
-<h4><span class="red">Early Registration Discount Expires 25 February 2016</span></h4>
-
-<p><strong>Late Registration Closes 24 March 2016 a week before the events commence and just
-
-before the Easter weekend.</strong></p>
-
-<p>Evo<span class="red">*</span> takes place on 30 March-1 April 2016 in Porto, Portugal </p>
-
-</applet><strong><a href="http://store.napier.ac.uk/browse/product.asp?compid=1&modid=1&catid=306">REGISTER HERE</a></strong>
-
-
-<p>Evo<span class="red">Star</span>2016 is the name for the four co-located conferences of Euro<span class="red">GP</span>, Evo<span class="red">COP</span>, Evo<span class="red">MusArt</span> and also Evo<span class="red">Applications</span> including conference tracks of Evo<span class="red">BIO</span>, Evo<span class="red">COMNET</span>, Evo<span class="red">COMPLEX</span>, Evo<span class="red">ENERGY</span>, Evo<span class="red">FIN</span>, Evo<span class="red">GAMES</span>, Evo<span class="red">IASP</span>, Evo<span class="red">INDUSTRY</span>, Evo<span class="red">NUM</span>, Evo<span class="red">PAR</span>, Evo<span class="red">RISK</span>, Evo<span class="red">ROBOT</span>, Evo<span class="red">STOC</span>.  These co-located events will be held in Porto, Portugal on 30 March 1 April 2016.</p>
-
-<p>The single registration fee allows access to all events over three days and includes lunches and coffee breaks at the conference venue; one ticket for the conference reception and one ticket for the conference dinner (additional tickets available for purchase).  Also included is a USB stick with all EvoStar2016 conference proceedings and electronic access for these LNCS volumes will be available for a limited period before and after the EvoStar events via www.springerlink.com.  Hard copy volumes are <strong>NOT</strong> included as part of the registration fee but are available for separate purchase directly from Springer.</p>
-
-<p>The friendly and informal atmosphere at EvoStar is intended to foster beneficial friendships and longer-term collaboration across continents and across research generations.</p>
-
-<p><strong>The early booking discount is given to all registrations received by 25 February 2016</strong>, and you are encouraged to take advantage of the lower fees.  The reduced registration rate will automatically cease after 1 March and thereafter registrations will be charged at the higher rate (no exceptions). The organisers work hard to minimise conference fees, and the 2016 registration rates represent a decrease to previous rates in real terms.  Conference registrations are available on a first-come, first-served basis, with the organisers reserving the right to limit places if necessary. <strong>Registrations close on 24 March 2016</strong>, a week before the events commence and just before the Easter weekend. 
-
-<strong>Half price student discounts</strong> are available TO
-
-MATRICULATED STUDENTS from recognised academic institutions studying for degrees in 
-
-subjects relevant to evolutionary computing.  You MAY be asked to supply your student 
-
-number or confirmation from your director of studies to confirm you are entitled to the student
-
-<p>Online registration and payment are available via a SSL secure website operated by Edinburgh Napier University in conjunction with Thawte as verified by VISA and MASTERCARD.  Online payment can be made using VISA, MASTERCARD, VISA DEBIT, VISA ELECTRON.  Please note that payment is charged in British pounds sterling at the rates shown below, with Euro fees described as an approximate guide.  A different Euro rate may be charged by your bank or credit card company.  Payment can also be made by cheque in Euros or British pounds sterling (please contact <a href="j.willies@napier.ac.uk">Jennifer Willies</a>, EvoStar Conference Coordinator for more information).  <strong><span class="red">Please note that sending funds by bank transfer is not available for this conference.</span></strong>  </p>
-
-<h3><a href="http://store.napier.ac.uk/browse/product.asp?compid=1&modid=1&catid=306"><p>Register HERE  </p></a></h3>
-
-
-<div class="panel panel-default">
-            <div class="panel-heading">
-              <h2 class="panel-title">Evostar 2016 registration</h2>
-            </div>
-            <div class="panel-body">
-              
-             <strong>WHAT REGISTRATION INCLUDES</strong>
-
-The SINGLE registration for all EVO* events includes:
-<ul>
-<li>Attendance at all EvoStar sessions for all four conferences </li>
-<li>Lunches and coffee breaks at the conference venue over three days</li>
-<li>One ticket for the conference reception </li>
-<li>One ticket for the conference dinner (additional tickets available for purchase)</li>
-<li>One USB stick including all EvoStar2016 conference proceedings</li>
-<li>Electronic access via www.springerlink.com for all four EvoStar2016 LNCS volumes for a limited period before and after the EvoStar events.  Please note that hard copies of the proceedings are NOT included in the registration fee.</li>
-
+   Online registrations and payment will be available very soon.  The SINGLE EvoStar registration includes:
+   <ul>
+   <li>Attendance at all EvoStar sessions for all four conferences</li>
+<li>   Lunches and coffee breaks at the conference venue over three days</li>
+<li>   One ticket for the conference reception</li>
+<li>   One ticket for the conference dinner (additional tickets available for purchase)</li>
+<li>   Electronic access to the EvoStar2017 conference proceedings published by Springer in their LNCS series.  Please note that hard copy volumes are NOT included in the registration fee but are available for separate purchase directly from Springer</li>
+<li>   Annual membership of the <a href="http://species-society.dei.uc.pt/">SPECIES Society</a>, promoting evolutionary computation in Europe</li>
 </ul>
-</h3>REGISTRATION FEES</h3>
-
-Online payment is charged in GBP, and these Euro rates are approximate, and for general guidance only.
-
 
 <table class="table table-bordered">
 	<tr>
-		<td></td><td><strong><span class="red">EARLY Registration until 25 February 2016</span></strong></td><td>LATE Registration from 26 February-24 March 2016</td>
+		<td>Fees</td><td><strong><span class="red">EARLY Registration until 1 March 2017</span></strong></td><td>LATE Registration from 2 March-10 April 2017</td>
 	</tr>
 
 	<tr>
-		<td>Student</td><td class="success"><strong>190 GBP</strong> (approx 250 EUR)</td><td>285 GBP (approx 375 EUR)</td>
+		<td>Regular</td><td class="success"><strong>550 EUR</strong> </td><td>750 EUR</td>
 	</tr>
 
 	<tr>
-		<td>Regular</td><td class="success"><strong>375 GBP</strong> (approx 500 EUR)</td><td>565 GBP (approx 750 EUR)</td>
+		<td>Student</td><td class="success"><strong>275 EUR</strong> </td><td>375 EUR</td>
 	</tr>
 </table>
 
-<h3>ADDITIONAL ITEMS YOU CAN ORDER</h3>
 
-Additional conference dinner tickets for accompanying persons may be purchased for 30 GBP (approx 40 EUR).
-
-
-<h3>HOW TO REGISTER AND PAY</h3>
-
-<p>
-Online registration and payment are available via a SSL secure website operated by Edinburgh Napier University in conjunction with Barclaycard.  Online payment can be made using VISA, MASTERCARD, VISA DEBIT, VISA ELECTRON.  Please note that payment is charged in British pounds sterling at the rates shown below, with Euro fees described as an <em>approximate</em> guide.  A different Euro amount may be charged by your bank or credit card company.  Payment can also be made by cheque in Euros or British pounds sterling (please contact <a href="mailto:j.willies@napier.ac.uk">Jennifer Willies, EvoStar Conference Coordinator</a> for more information). <strong> <span class="red"> Please note that sending funds by bank transfer is NOT available for EvoStar, payment only by credit card or EUR/GBP cheque.</span> </strong> 
-</p>
-<p>
-<a href="http://store.napier.ac.uk/browse/product.asp?compid=1&modid=1&catid=306">Register for EvoStar 2016 Now  </a>
-</p>
-
-<h3>FURTHER INFORMATION</h3>
-From <a href="mailto:j.willies@napier.ac.uk">Jennifer Willies</a>, EvoStar Conference Coordinator 
-              
-            </div>
-      </div>
-
--->
 
 <?php include('footer.php') ?>
