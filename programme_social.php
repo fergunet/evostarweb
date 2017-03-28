270490ff8e526e5f6b0d17d58ff5c081a3c69382
Pablo García Sánchez <fergunet@gmail.com>
2017-03-28 12:51:11 +0200

    Forgot to add social

---
+++ programme_social.php
@@ -0,0 +1,10 @@
+
+<?php include('header.php') ?>
+
+        <h3>Evostar <span class="red">Social Programme</span> <h3>
+   </div>
+   
+   <p>On Friday afternoon 21 April, EvoStar participants are invited join an optional canal cruise which passes by many famous Amsterdam historic sights.  
+     Free vouchers are available at the Registration Desk which entitle EvoStar participants to the 100 Highlights Canal Cruise which lasts an hour and departs approximately every 15 minutes from <strong>Prins Hendrikkade 33a (opposite Centraal Station)</strong>.   More details available from the Registration Desk.</p>
+
+<?php include('footer.php') ?>
