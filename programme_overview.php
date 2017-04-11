3421199016a30fa374a282ac2145fcc8bbabfd5c
Pablo García Sánchez <fergunet@gmail.com>
2017-04-11 15:11:39 +0200

    Added programme info

--- programme_overview.php
+++ programme_overview.php
@@ -1,16 +1,16 @@
 
 <?php include('header.php') ?>
 
-        <h3>Evostar <span class="red">Programme</span> <h3>
+        <h2>Evostar <span class="red">Programme</span> </h2>
    </div>
    
    <h3><a href="pdfs/StarProgDraft.pdf">Draft online programme</a></h3>
-   <!--
-   <h3><a href="pdfs/EvoStarHandbook2016reduced.pdf">Conference handbook</a></h3>
-<a href="pdfs/OverviewProgramme.pdf"><h3>Sessions schedule</h3></a>
-<h3>Euro<span class="red">GP</span>: <a href="pdfs/EuroGPprogramme.pdf">Full programme</a>. <a href="pdfs/EuroGPschedule.pdf">[Schedule]</a></h3></a>
-<h3>Evo<span class="red">MUSART</span>: <a href="pdfs/EvoMUSARTprogramme.pdf">Full programme</a>. <a href="pdfs/EvoMUSARTschedule.pdf">[Schedule]</a></h3></a>
-<h3>Evo<span class="red">COP</span>: <a href="pdfs/EvoCOPprogramme.pdf">Full programme</a>. <a href="pdfs/EvoCOPschedule.pdf">[Schedule]</a></h3></a>
-<h3>Evo<span class="red">APPS</span>: <a href="pdfs/EvoAPPSprogramme.pdf">Full programme</a>. <a href="pdfs/EvoAPPSschedule.pdf">[Schedule]</a></h3></a>
--->
+   
+   <!--<h3><a href="pdfs/EvoStarHandbook2016reduced.pdf">Conference handbook</a></h3>
+<a href="pdfs/OverviewProgramme.pdf"><h3>Sessions schedule</h3></a>-->
+<h3>Euro<span class="red">GP</span>: <a href="pdfs/EuroGPfinalprog.pdf">Full programme</a>. </h3>
+<h3>Evo<span class="red">MUSART</span>: <a href="pdfs/EvoMUSARTfinalprog.pdf">Full programme</a>.</h3>
+<h3>Evo<span class="red">COP</span>: <a href="pdfs/EvoCOPfinalprog.pdf">Full programme</a>. </h3>
+<h3>Evo<span class="red">APPS</span>: <a href="pdfs/EvoAPPSfinalprog.pdf">Full programme</a>. </h3>
+
 <?php include('footer.php') ?>
