a17cbf5d2d951ab6ad8c2abf60eda8f20fe569bd
Pablo García Sánchez <fergunet@gmail.com>
2017-04-11 15:15:58 +0200

    More changes adding the programme

--- programme_overview.php
+++ programme_overview.php
@@ -6,8 +6,8 @@
    
    <h3><a href="pdfs/StarProgDraft.pdf">Draft online programme</a></h3>
    
-   <!--<h3><a href="pdfs/EvoStarHandbook2016reduced.pdf">Conference handbook</a></h3>
-<a href="pdfs/OverviewProgramme.pdf"><h3>Sessions schedule</h3></a>-->
+   <h3><a href="pdfs/EvoStarConferenceHandbook.pdf">Conference handbook</a></h3>
+<!--<a href="pdfs/OverviewProgramme.pdf"><h3>Sessions schedule</h3></a>-->
 <h3>Euro<span class="red">GP</span>: <a href="pdfs/EuroGPfinalprog.pdf">Full programme</a>. </h3>
 <h3>Evo<span class="red">MUSART</span>: <a href="pdfs/EvoMUSARTfinalprog.pdf">Full programme</a>.</h3>
 <h3>Evo<span class="red">COP</span>: <a href="pdfs/EvoCOPfinalprog.pdf">Full programme</a>. </h3>
