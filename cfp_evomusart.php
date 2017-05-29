06099e2a9b00d6bbeeb2745fb14bf325e348c80f
Pablo García Sánchez <fergunet@gmail.com>
2017-05-29 13:22:49 +0200

    Added two organisers and also modifications to evomusart

--- cfp_evomusart.php
+++ cfp_evomusart.php
@@ -91,11 +91,12 @@ TBA
 
 
 <h3>Publication Details</h3>
-</p><p>
-Submissions will be rigorously reviewed for scientific and artistic merit. Accepted papers will be presented orally or as posters at the event and included in the evostar proceedings, published by Springer Verlag in a dedicated volume of the Lecture Notes in Computer Science series. The acceptance rate at evo<span class="red">MUSART</span> 2016 was 40% for papers accepted for oral presentation, and 24% for poster presentation.
-</p><p>
-Submitters are strongly encouraged to provide in all papers a link for download of media demonstrating their results, whether music, images, video, or other media types. Links should be anonymised for double-blind review, e.g. using a URL shortening service.
-
+<p>
+	Submissions will be rigorously reviewed for scientific and artistic merit. Accepted papers will be presented orally or as posters at the event and included in the evostar proceedings, published by Springer Verlag in a dedicated volume of the Lecture Notes in Computer Science series. The acceptance rate at evoMUSART 2017 was 41% for papers accepted for oral presentation, and 41% for poster presentation.
+</p>
+<p>
+	Submitters are strongly encouraged to provide in all papers a link for download of media demonstrating their results, whether music, images, video, or other media types. Links should be anonymised for double-blind review, e.g. using a URL shortening service.
+</p>
 <h3>Additional information and submission details</h3>
 
 Submit your manuscript, at most 16 A4 pages long, in Springer LNCS format (instructions downloadable from <a href="http://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0">http://www.springer.com/computer/lncs?SGWID=0-164-6-793341-0</a>)<br/>
