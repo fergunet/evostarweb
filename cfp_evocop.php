51aa8cf70ab546c304fdbdf28973b4c3a4831e80
Pablo García Sánchez <fergunet@gmail.com>
2017-10-12 15:04:28 +0200

    Changes to EvoCop

--- cfp_evocop.php
+++ cfp_evocop.php
@@ -12,54 +12,76 @@
       including: Euro<span class="red">GP</span>, Evo<span class="red">COP</span>, Evo<span class="red">MUSART</span> and 
       Evo<span class="red">Applications</span><br />
     </p>
-    <p>The 18th European Conference on Evolutionary Computation in Combinatorial Optimisation is a multidisciplinary conference that brings together researchers working on metaheuristics for solving difficult combinatorial optimisation problems appearing in various industrial, economic, and scientific domains. Prominent examples of metaheuristics include: evolutionary algorithms, simulated annealing, tabu search, scatter search and path relinking, memetic algorithms, ant colony and bee colony optimisation, particle swarm optimisation, variable neighbourhood search, iterated local search, greedy randomized adaptive search procedures, estimation of distribution algorithms, and hyperheuristics. Successfully solved problems include scheduling, timetabling, network design, transportation and distribution problems, vehicle routing, travelling salesman, graph problems, satisfiability, energy optimisation problems, packing problems, and planning problems.</p>
+    <p>The 18th European Conference on Evolutionary Computation in
+Combinatorial Optimisation is a multidisciplinary conference that
+brings together researchers working on evolutionary computation
+methods and other metaheuristics for solving difficult combinatorial
+optimisation problems appearing in various industrial, economic, and
+scientific domains. Prominent examples of metaheuristics include:
+evolutionary algorithms, estimation of distribution algorithms, swarm
+intelligence methods such as ant colony and particle swarm
+optimisation, local search methods such as simulated annealing, tabu
+search, variable neighbourhood search, iterated local search, scatter
+search and path relinking, and their hybridisation, such as memetic
+algorithms. Automatic algorithm configuration and design,
+meta-optimisation, model-based methods, and hyperheuristics are also
+topics of interest. Successfully solved problems include, but are not
+limited to, multi-objective, uncertain, dynamic and stochastic
+problems in the context of scheduling, timetabling, network design,
+transportation and distribution, vehicle routing, graph problems,
+satisfiability, energy optimisation, cutting, packing, and planning
+problems.</p>
 
 
-      <p>The Evo<span class="red">COP</span> 2018 conference will be held in the city of Parma, Italy. It will be held in conjunction with Euro<span class="red">GP</span> (the 19th European Conference on Genetic Programming), Evo<span class="red">MUSART</span> (5th European conference on evolutionary and biologically inspired music, sound, art and design) and Evo<span class="red">Applications</span> (specialist events on a range of evolutionary computation topics and applications), in a joint event collectively known as Evo<span class="red">*</span>.</p>
+      <p>The EvoCOP 2018 conference will be held in the city of Parma, Italy,
+together with EuroGP (the 21st European Conference on Genetic
+Programming), EvoMUSART (7th European conference on evolutionary and
+biologically inspired music, sound, art and design) and
+EvoApplications (specialist events on a range of evolutionary
+computation topics and applications), in a joint event collectively
+known as EvoStar (Evo*).</p>
 
     <p><strong>Areas of Interest and Contributions</strong></p>
     <p>Topics of interest include, but are not limited to:<br />
     </p>
     <ul>
-<li>Applications of metaheuristics to combinatorial optimisation problems</li>
-<li>Novel application domains for metaheuristic optimisation methods</li>
-<li>Representation techniques</li>
-<li>Neighbourhoods and efficient algorithms for searching them</li>
-<li>Variation operators for stochastic search methods</li>
-<li>Constraint-handling techniques</li>
-<li>Hybrid methods and hybridization techniques</li>
-<li>Parallelization</li>
-<li>Theoretical developments</li>
-<li>Search space and landscape analyses</li>
-<li>Comparisons between different (also exact) techniques</li>
-<li>Metaheuristics and machine learning</li>
-<li>Ant colony optimisation</li>
-<li>Artificial immune systems</li>
-<li>Genetic programming and Genetic algorithms</li>
-<li>(Meta-)heuristics</li>
-<li>Scatter search</li>
-<li>Particle swarm optimisation</li>
-<li>Tabu search, iterated local search and variable neighbourhood search</li>
-<li>Memetic algorithms</li>
-<li>Hyper-heuristics and autonomous search</li>
-<li>Estimation of distribution algorithms</li>
-<li>String processing</li>
-<li>Scheduling and timetabling</li>
-<li>Network design</li>
-<li>Vehicle routing</li>
-<li>Graph problems</li>
-<li>Satisfiability</li>
-<li>Packing and cutting problems</li>
-<li>Energy optimisation problems</li>
-<li>Practical solving of NP-hard problems</li>
-<li>Mixed integer programming</li>
-<li>Multi-objective optimisation</li>
-<li>Grid computing</li>
-<li>Combinatorial optimisation</li>
-<li>Nature and Bio-inspired methods</li>
-<li>Quantum computing and quantum annealing</li>
-<li>Optimisation in Cloud computing</li>
-<li>Search-based software engineering</li>
+
+ <li> Applications of metaheuristics to combinatorial optimisation problems</li>
+ <li> Representation techniques</li>
+ <li> Practical solution of NP-hard problems</li>
+ <li> Neighbourhoods and efficient algorithms for searching them</li>
+ <li> Variation operators for stochastic search methods</li>
+ <li> Theoretical developments</li>
+ <li> Constraint-handling techniques</li>
+ <li> Parallelisation and grid computing</li>
+ <li> Search space and landscape analyses</li>
+ <li> Comparisons between different (also exact) methods</li>
+ <li> Heuristics</li>
+ <li> Genetic programming and Genetic algorithms</li>
+ <li> Tabu search, iterated local search and variable neighbourhood search</li>
+ <li> Ant colony optimisation</li>
+ <li> Artificial immune systems</li>
+ <li> Scatter search</li>
+ <li> Particle swarm optimisation</li>
+ <li> Memetic algorithms</li>
+ <li> Hybrid methods and hybridisation techniques</li>
+ <li> Matheuristics (hybrids of exact and heuristic methods)</li>
+ <li> Hyper-heuristics and autonomous search</li>
+ <li> Automatic algorithm configuration and design</li>
+ <li> Metaheuristics and machine learning</li>
+ <li> Surrogate-model-based methods</li>
+ <li> Estimation of distribution algorithms</li>
+ <li> String processing</li>
+ <li> Scheduling and timetabling</li>
+ <li> Network design</li>
+ <li> Vehicle routing</li>
+ <li> Graph problems</li>
+ <li> Satisfiability</li>
+ <li> Packing and cutting problems</li>
+ <li> Energy optimisation problems</li>
+ <li> Multi-objective optimisation</li>
+ <li> Search-based software engineering</li>
+
     </ul>
 
 
@@ -67,8 +89,8 @@
     <h3>Evo<span class="red">COP</span> Programme Chairs</h3>
 
 <ul>
-  <li><strong>Manuel López-Ibáñez</strong>, University of Manchester, UK, manuel.lopez-ibanez (at) manchester.ac.uk</li>
   <li><strong>Arnaud Liefooghe</strong>, University of Lille, France, arnaud.liefooghe (at) univ-lille1.fr</li>
+<li><strong>Manuel López-Ibáñez</strong>, University of Manchester, UK, manuel.lopez-ibanez (at) manchester.ac.uk</li>
 </ul>
 
 
@@ -76,8 +98,81 @@
 
 
 
-
- 
+<h3>Programme Committee</h3>
+<ul>
+<li> Adnan Acan, Eastern Mediterranean University, Turkey</li>
+<li> Hernán Aguirre, Shinshu University, France</li>
+<li> Enrique Alba, Universidad de Málaga, Spain</li>
+<li> Richard Allmendinger, University of Manchester, UK</li>
+<li> Thomas Bartz-Beielstein, Cologne University of Applied Sciences, Germany</li>
+<li> Hans-Georg Beyer, Vorarlberg University of Applied Sciences, Germany</li>
+<li> Benjamin Biesinger, Austrian Institute of Technology, Austria</li>
+<li> Christian Blum, Artificial Intelligence Research Institute (IIIA-CSIC), Bellaterra, Spain</li>
+<li> Sandy Brownlee, University of Stirling, UK</li>
+<li> Pedro Castillo, Universidad de Granada, Spain</li>
+<li> Francisco Chicano, Universidad de Malaga, Spain</li>
+<li> Carlos Coello Coello, CINVESTAV-IPN, Mexico</li>
+<li> Peter Cowling, University of Bradford, UK</li>
+<li> Bilel Derbel, University of Lille, France</li>
+<li> Luca Di Gaspero, University of Udine, Italy</li>
+<li> Karl Doerner, Johannes Kepler University Linz, Austria</li>
+<li> Benjamin Doerr, LIX-Ecole Polytechnique, France</li>
+<li> Carola Doerr, Max Planck Institute for Informatics, Germany</li>
+<li> Paola Festa, Universitá di Napoli Federico II, Italy</li>
+<li> Bernd Freisleben, University of Marburg, Germany</li>
+<li> Carlos Garcia-Martinez, University of Córdoba, Spain</li>
+<li> Adrien Goeffon, University of Angers, France</li>
+<li> Jens Gottlieb, SAP, Germany</li>
+<li> Walter Gutjahr, University of Vienna, Austria</li>
+<li> Said Hanafi, University of Valenciennes, France</li>
+<li> Jin-Kao Hao, University of Angers, France</li>
+<li> Emma Hart, Edinburgh Napier University, UK</li>
+<li> Geir Hasle, SINTEF Applied Mathematics, Norway</li>
+<li> Bin Hu, Austrian Institute of Technology, Austria</li>
+<li> Andrzej Jaszkiewicz, Poznan University of Technology, Poland</li>
+<li> Istvan Juhos, University of Szeged, Hungary</li>
+<li> Graham Kendall, University of Nottingham, UK</li>
+<li> Ahmed Kheiri, Lancaster University, UK</li>
+<li> Mario Koeppen, Kyushu Institute of Technology, Japan</li>
+<li> Frederic Lardeux, University of Angers, France</li>
+<li> Rhyd Lewis, Cardiff University, UK</li>
+<li> Arnaud Liefooghe, University of Lille, France</li>
+<li> Manuel López-Ibáñez, University of Manchester, UK</li>
+<li> Jose Antonio Lozano, University of the Basque Country, Spain</li>
+<li> Gabriel Luque, University of Malaga, Spain</li>
+<li> David Meignan, University of Osnabruck, Germany</li>
+<li> Juan Julian Merelo, University of Granada, Spain</li>
+<li> Krzysztof Michalak, University of Economics, Wroclaw, Poland</li>
+<li> Martin Middendorf, University of Leipzig, Germany</li>
+<li> Christine L. Mumford, Cardiff University, UK</li>
+<li> Nysret Musliu, Vienna University of Technology, Austria</li>
+<li> Gabriela Ochoa, University of Stirling, UK</li>
+<li> Beatrice Ombuki-Berman, Brock University, Canada</li>
+<li> Luis Paquete, University of Coimbra, Portugal</li>
+<li> Mario Pavone, University of Catania, Italy</li>
+<li> Paola Pellegrini, French institute of science and technology for transport, France</li>
+<li> Francisco J. Pereira, Universidade de Coimbra, Portugal</li>
+<li> Matthias Prandtstetter, Austrian Institute of Technology, Austria</li>
+<li> Jakob Puchinger, SystemX-Centrale Supélec, France</li>
+<li> Günther Raidl, Vienna University of Technology, Austria</li>
+<li> Maria Cristina Riff, Universidad Técnica Federico Santa María, Chile</li>
+<li> Eduardo Rodriguez-Tello, CINVESTAV – Tamaulipas, Mexico</li>
+<li> Andrea Roli, Università di Bologna, Italy</li>
+<li> Peter Ross, Edinburgh Napier University, UK</li>
+<li> Frederic Saubion, University of Angers, France</li>
+<li> Patrick Siarry, University of Paris 12, France</li>
+<li> Kevin Sim, Edinburgh Napier University, UK</li>
+<li> Jim Smith, University of the West of England, UK</li>
+<li> Giovanni Squillero, Politecnico di Torino, Italy</li>
+<li> Thomas Stuetzle, Université Libre de Bruxelles, Belgium</li>
+<li> El-ghazali Talbi, University of Lille, France</li>
+<li> Sara Tari, University of Angers, France</li>
+<li> Renato Tinós, University of Sao Paulo, Brasil</li>
+<li> Nadarajen Veerapen, University of Stirling, UK</li>
+<li> Sébastien Verel, Université du Littoral Cote d'Opale, France</li>
+<li> Bing Xue, Victoria University of Wellington, New Zealand</li>
+<li> Takeshi Yamada, NTT Communication Science Laboratories, Japan</li>
+</ul>
 
 
 
@@ -94,19 +189,54 @@
 
 
     <p><strong>Publication Details</strong></p>
-    <p>All accepted papers will be presented orally at the conference and printed 
-      in the proceedings published by Springer in the LNCS series (see LNCS volumes 
-      2037, 2279, 2611, 3004, 3448, 3906, 4446, 4972, 5482, 6022, 6622, 7245, 
-      7832, 8600 and 9026 for the previous proceedings).</p>
-    <p><strong>Submission Details</strong></p>
-    <p>Submissions must be original and not published elsewhere. The submissions will be peer reviewed by members of the program committee. The authors of accepted papers will have to improve their paper on the basis of the reviewers comments and will be asked to send a camera ready version of their manuscripts. At least one author of each accepted work has to register for the conference, attend the conference and present the work.</p>
-<p>
-The reviewing process will be double-blind, please omit information about the authors in the submitted paper. Submit your manuscript in Springer LNCS format.</p>
-    <p>
-      Page limit: <strong>16 pages</strong>
+<p>Regular papers will be presented orally at the conference and
+printed in the proceedings published by Springer in the LNCS series
+(see LNCS volumes 2037, 2279, 2611, 3004, 3448, 3906, 4446, 4972,
+5482, 6022, 6622, 7245, 7832, 8600, 9026, 9595 and 10197 for the
+previous proceedings).</p>
+
+<p>Late-breaking abstracts (LBAs) summarising ongoing research, recent
+studies and applications of evolutionary computation and other
+meta-heuristics to real-world or academic combinatorial optimisation
+problems. LBAs will be presented as posters during the conference.
 </p>
+
+    <p><strong>Submission Details</strong></p>
+<p>Regular paper submissions must be original and not published
+elsewhere. The submissions will be peer reviewed by members of the
+program committee. The authors of accepted papers will have to improve
+their paper on the basis of the reviewers' comments and will be asked
+to send a camera ready version of their manuscripts. At least one
+author of each accepted work has to register for the conference,
+attend the conference and present the work.</p>
+<p>
+The reviewing process will be double-blind, please omit information
+about the authors in the submitted paper. Submit your manuscript in
+Springer LNCS format.
+</p>
+<ul>
+<li>Page limit (Regular papers): 16 pages</li>
+<li>Page limit (LBAs): 1 page</li>
+</ul>
+Submission link: http://myreview.saclay.inria.fr/evocop18/
 <p><strong>Submission link</strong>: <a href="https://myreview.saclay.inria.fr/evocop18/">https://myreview.saclay.inria.fr/evocop18/</a><br /></p>
 
+<p><strong>Important Dates</strong></p>
+<ul>
+<li>Submission deadline (Regular papers): November 1, 2017</li>
+<li>Notification (Regular papers): 3 January 2018</li>
+<li>Camera-ready (Regular papers): 15 January 2018</li>
+
+<li>Submission deadline (Late-breaking abstracts): January 15, 2018</li>
+<li>Notification (Late-breaking abstracts): TBA</li>
+<li>Camera-ready (Late-breaking abstracts): TBA</li>
+
+<li>Mandatory registration per paper: 1 February 2017</li>
+<li>Student bursary deadline: TBA</li>
+<li>Early registration discount: TBA</li>
+<li>Registration deadline: TBA</li>
+<li>EvoStar dates: 4-6 April 2018</li>
+</ul>
 
 
     <?php include('footer.php') ?>
