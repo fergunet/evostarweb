69295caf5d1bfc3c0ecae237aca893c460eba5ba
Pablo García Sánchez <fergunet@gmail.com>
2017-10-13 13:15:09 +0200

    Added submission links

--- submission_details.php
+++ submission_details.php
@@ -6,7 +6,10 @@
 
         <strong><a href="submission_camera_ready.php">Press here for CAMERA READY instructions to send the final version of your paper AFTER acceptance.</a></strong>
         <h3>Submission links</h3>
-TBA
-
-
+        <ul>
+<li>Submission link for EuroGP: <a href="https://myreview.saclay.inria.fr/eurogp18">https://myreview.saclay.inria.fr/eurogp18</a></li>
+<li>Submission link for EvoCOP: <a href="https://myreview.saclay.inria.fr/evocop18/">https://myreview.saclay.inria.fr/evocop18/</a></li>
+<li>Submission link for EvoMUSART: <a href="https://myreview.saclay.inria.fr/evomusart18">https://myreview.saclay.inria.fr/evomusart18</a></li>
+<li>Submission link for EvoAPPS: <a href="https://myreview.saclay.inria.fr/evoapps18/">https://myreview.saclay.inria.fr/evoapps18/</a></li>
+</ul>
     <?php include('footer.php') ?>
