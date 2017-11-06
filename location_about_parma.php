9220719511baaec7549df286b77a42261b859fb5
Pablo García Sánchez <fergunet@gmail.com>
2017-11-06 13:42:11 +0100

    Added info about parma

--- location_about_parma.php
+++ location_about_parma.php
@@ -4,5 +4,21 @@
    </div>
 
 
-TBA
+   <p>Parma is located in northern Italy, in Emilia-Romagna, halfway between Bologna and
+   Milan.</p>
+   <p>Parma is an elegant city with a refined atmosphere, inherited from its 18th century
+   legacy with the French royal family (the central Parco Ducale reproduces the design of
+   the Jardin des Tuileries in Paris), that can only be breathed in a “petite capitale”. With
+   its center rich of art, parks and treasures from different ages, Parma is a very
+   welcoming place.</p>
+   <p>
+   Parma is known for its gastronomic heritage, has been selected as “food creativity
+   town” by UNESCO, and is world-famous for products like Parmigiano Reggiano
+   cheese and Parma ham.</p>
+   <p>
+   Detailed tourist information can be found at <a href="http://turismo.comune.parma.it/en">http://turismo.comune.parma.it/en</a>
+   The conference site is the historical central building of the Univeristy in via Università
+   1, right in the city center and at walking distance from most central hotels.</p>
+
+   Information about <a href="location_parma.php">getting to parma is available here</a>.
  <?php include('footer.php') ?>
--- location_parma.php
+++ location_parma.php
@@ -1,7 +1,75 @@
 <?php include('header.php') ?>
 
-        <h2>Getting About <span class="red">Parma</span><h2>
+        <h2>Getting About <span class="red">Parma</span></h2>
    </div>
 
-TBA
+
+<h3>By plane:</h3> From the “Giuseppe Verdi” airport, only 5 km away from the heart of the city,
+the historical centre can be easily reached by taxi, rental cars or by bus n. 6.
+Unfortunately only very few flights reach Parma directly.
+
+<p>
+More likely landing airports are Milan &quot;Malpensa&quot;, Milan &quot;Linate&quot;, Milan &quot;Orio al Serio&quot;
+(located in Bergamo, hub for many low-cost airlines) and Bologna &quot;G. Marconi&quot;
+(advised).</p>
+<p>
+All these airport provide shuttle bus services to the corresponding central stations in
+Milan or Bologna (5 to 7 Euros), running until around 11 pm. Tickets are sold on board.</p>
+
+<h3>By train:</h3> Train connections to Parma from &quot;Bologna Centrale&quot; and &quot;Milano Centrale&quot;
+are very frequent and run from the very early morning to around 11 pm. Travel times
+are from 45 minutes (Trenitalia Frecciabianca trains) to 70 minutes (very cheap
+regional trains) to/from Bologna and from 55 to 80 minutes to/from Milan.
+Tickets can be bought at vending machines or ticket booths inside the stations or even
+at tobacconists or newspaper kiosks (regional trains).
+<p>
+Train time-tables can be found at <a href="http://www.trenitalia.com/tcom-en">http://www.trenitalia.com/tcom-en</a> (regional and
+high-speed connections by Trenitalia) or <a href="http://www.italotreno.it/en">http://www.italotreno.it/en</a> (high-speed
+connections by NTV).</p>
+<p>
+High-speed trains (Trenitalia Frecciarossa or Frecciargento, NTV Italo) reach Bologna
+Centrale station or Reggio Emilia Mediopadana station. Bologna Centrale can be
+reached from Rome in little more than 2 hours and from Florence in 35 minutes,
+Reggio Emilia in 2.5 hours from Rome and 70 minutes from Florence.
+</p>
+<p>
+From Reggio Emilia Mediopadana high-speed train station, Parma can be reached by
+the Italobus service (included in the train price if you have traveled by NTV Italo high-
+speed train). Be aware that no direct train connections exist between Reggio Emilia
+Mediopadana station and Parma station.</p>
+<p>
+Parma railway station is in the North of the city, 10 minutes&#39; walk from the city center
+and at walking distance (up to 15 minutes) to most hotels.
+</p>
+
+<h3>By car:</h3> Parma is located along the motorway A1 Milan-Bologna and along the A15
+Parma-La Spezia, Parma has two motorway exits:
+<ul>
+<li>1. exit &quot;Parma Ovest&quot;, from A15, about 10 Kms from the city;</li>
+<li>2. exit &quot;Parma&quot; from A1.</li>
+</ul>
+<p>
+Located at the access points of the town there are the exchange car parks, free and
+open 24 hours a day. You can leave your car there and reach the city center by shuttle
+buses.</p>
+<p>
+Indoor toll parking is available closer to the center of town, as well as along the city
+streets, marked by blue lines: toll parking in the mornings and afternoons using park
+meters, free around lunchtime and at night. Prices may vary depending on the location.
+</p>
+<p>
+The historical center of Parma is a Limited Traffic Zone (ZTL) controlled by cameras
+working every day, including festivities. During the day, access to the city center is
+allowed only to residents or to foreigners lodging in the hotels in the center.
+</p>
+<h3>Beyond Parma...</h3>
+<p>
+The city’s central location within Italy makes it easy to reach the sea, the Alps or the
+most famous Italian art cities from Parma, thanks to excellent motorway and railway
+connections.
+</p>
+<p>
+Rome, Florence, and Venice are about 3h, 1.5h, and 2.5h away from Parma by high-
+speed train, or about 4.5 h, 2h, 3h by car, respectively.
+</p>
     <?php include('footer.php') ?>
