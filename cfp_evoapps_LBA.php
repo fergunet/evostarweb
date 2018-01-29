970bf7d1e8caf41534a8da1c3044b05857ed9cda
Pablo García Sánchez <fergunet@gmail.com>
2018-01-29 10:42:41 +0100

    Added apps LBA

---
+++ cfp_evoapps_LBA.php
@@ -0,0 +1,33 @@
+ <?php include('header.php') ?>
+
+        <h2>Evo<span class="red">Applications Late-Breaking Abstracts</span><h2>
+   </div>
+
+<p>
+EvoAPPS 2018 invites submissions of late-breaking abstracts (LBAs) summarising ongoing research and recent studies in all areas of applications of Evolutionary Computation and other Nature-inspired techniques.
+</p><p>
+LBAs will be presented as posters during the conference and published online (in CEUR or arXiv).
+</p><p>
+At least one author of each accepted work has to register for the conference, attend the conference and present the work.
+</p><p>
+Papers will be reviewed by the organising committee to ensure quality and relevance to the conference.
+</p><p>
+Submit your manuscript in Springer LNCS format. Page limit (LBAs): 4 pages.
+</p>
+Submit your LBA via <a href="https://myreview.saclay.inria.fr/evoapps18_lba/">https://myreview.saclay.inria.fr/evoapps18_lba/</a>
+
+<h3>Important Dates</h3>
+
+<li> Submission deadline (LBAs): 12 February 2018</li>
+<li> Notification (LBAs): 16 February 2018</li>
+<li> Mandatory registration per LBA: 28 February 2018</li>
+<li> EvoStar: April 4-6, 2018</li>
+
+<p>Regards</p>
+
+EvoApplications Chairs
+
+
+
+
+ <?php include('footer.php') ?>
