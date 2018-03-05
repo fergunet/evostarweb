57b7436255af8840791050ac9f027d7df37cb363
Pablo García Sánchez <fergunet@gmail.com>
2018-03-05 21:17:20 +0100

    Added Una May photo

--- programme_speakers.php
+++ programme_speakers.php
@@ -8,7 +8,7 @@
 
 
 <div>
-<img src="img/logo2018.png" width="200px" align="left" style="margin:10px"/>
+<img src="img/unamay.jpg" width="200px" align="left" style="margin:10px"/>
 <strong>Abstract:</strong> The world abounds with adversarial relationships - opponents squaring off in contests and disputes. They are found in nature (predator vs prey) and in human society (gaming, wars, cyber security). They shape important outcomes and influence the character of our existence. I will describe  my research agenda on the dynamics of adversarial relationships and understanding how optimization and evolutionary computation comprise a useful lens through which they can be examined.
 </div>
 <hr/>
