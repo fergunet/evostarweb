544a286522d685f837cb0b2d5b02d7dd44c11705
Pablo García Sánchez <fergunet@gmail.com>
2018-03-19 13:52:05 +0100

    More changes in scavenger, posters and social

--- location_posters.php
+++ location_posters.php
@@ -2,6 +2,42 @@
 
         <h2>Posters/<span class="red">Wifi</span>:<h2>
    </div>
-TBA
+<h4>AV at the venue</h4>
+You are encouraged to bring your own laptop or tablet for your presentation, along with
+appropriate adaptors and chargers for your equipment as EvoStar does not provide these.
+All LCD projectors (beamers) have a VGA input. Some may provide HDMI, but this is not
+guaranteed. Voltage in Italy is 220v, and the two-pin plug is used, similar to most
+European countries.
+
+<h4>POSTERS</h4>
+The poster session will take place in conjunction with the EvoStar reception on
+Wednesday evening, 4 April from 1830-2000. Poster boards are 1 metre wide by 2 metres
+tall and can accommodate posters up to A0 size. Tape can be used to fix posters and this
+will be available at the conference venue.
+<p>
+Poster tubes can be stored safely at the registration desk, but please remember to label
+your poster so it does not get lost or mixed up with others.
+</p>
+A service is available from the University of Parma to have your poster printed and
+delivered to the conference desk by lunchtime on Wednesday, 4 April. Prices are below
+and please ensure your PDF is sent to <strong class="red">evoposters@ce.unipr.it</strong>  <strong>no later than 28 March</strong>,
+otherwise delivery cannot be guaranteed. Payment in cash only on collection of poster.
+
+<p>
+<table border="1" style="width:100%; text-align:center; padding:15px;">
+
+<tr> <th>Format</th><th>        Paper type       </th><th>    Price (Euros, VAT 22% n.i.)</th></tr>
+<tr> <td>A0</td><td>        Cardboard 120g 	     </td><td>17.00</td></tr>
+<tr> <td>A0</td><td>        Cardboard 170g 	     </td><td>18.00</td></tr>
+<tr> <td>A0</td><td>        Photographic paper   </td><td>21.00</td></tr>
+<tr> <td>A1</td><td>        Cardboard 120g 	     </td><td>14.00</td></tr>
+<tr> <td>A1</td><td>        Cardboard 170g 	     </td><td>14.50</td></tr>
+<tr> <td>A1</td><td>        Photographic paper   </td><td>16.00</td></tr>
+</table>
+</p>
+<h4>WIFI</h4>
+WIFI is available at the conference venue. In addition to Eduroam being suitable for
+many, each participant will be assigned a unique account on the UNIPR network and your
+username details will be available on the backside of your conference badge.
 
    <?php include('footer.php') ?>
--- programme_scavenger.php
+++ programme_scavenger.php
@@ -8,5 +8,32 @@
 
 
 <h3>Instructions</h3>
-TBA
+
+All students attending EvoStar are invited to participate in the EvoStar 2018 Student
+Scavenger Hunt. Students have been organised in small teams to undertake these
+challenges below:
+<ul>
+<li>Post a picture of your team outdoors in Parma</li>
+<li>Submit the title of a paper that solves a real-world problem.</li>
+<li>Submit the title of a paper written by someone from Spain.</li>
+<li>Submit the title of a paper that has an application in games.</li>
+<li>Picture yourself with an attendee who has attended more than 10 EvoStar events.</li>
+<li>Post a picture of a member of your team presenting a paper.</li>
+<li>Post a picture of a presentation that covers the MAP Elites algorithm</li>
+<li>Post a picture of your team with a robot</li>
+<li>List the names of the SPECIES board members</li>
+<li>Upload a picture of each team member asking a question at the poster session</li>
+<li>Upload a picture of a team member with some Parma Ham or Parmesan cheese</li>
+<li>Picture your team with a member of the Species board who comes from Portugal</li>
+<li>Take a picture of your team after having seen a <a href="http://turismo.comune.parma.it/en/thematic-channels/discover-the-area/people-history-traditions/rites-legends/lucabaza">Lucabaza</a></li>
+</ul>
+To be eligible for prizes, be sure to provide your answers to <em>n.urquhart (AT) napier.ac.uk</em>
+no later than <strong>10am on Friday 6 April</strong>. Winners will be announced and prizes given out at
+the closing session.
+<p>
+EvoStar is grateful to Edinburgh Napier University for their support of student activities.
+</p>
+
+
+
 <?php include('footer.php') ?>
