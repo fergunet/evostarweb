e5396295961ac333d2700f1a5ac6de063987fd68
Pablo García Sánchez <fergunet@gmail.com>
2018-07-23 14:21:53 +0200

    Changes in evocop

--- cfp_evocop.php
+++ cfp_evocop.php
@@ -16,35 +16,31 @@
       including: Euro<span class="red">GP</span>, Evo<span class="red">COP</span>, Evo<span class="red">MUSART</span> and 
       Evo<span class="red">Applications</span><br />
     </p>
-    <p>The 19th European Conference on Evolutionary Computation in
-Combinatorial Optimisation is a multidisciplinary conference that
-brings together researchers working on evolutionary computation
-methods and other metaheuristics for solving difficult combinatorial
-optimisation problems appearing in various industrial, economic, and
-scientific domains. Prominent examples of metaheuristics include:
-evolutionary algorithms, estimation of distribution algorithms, swarm
-intelligence methods such as ant colony and particle swarm
-optimisation, local search methods such as simulated annealing, tabu
-search, variable neighbourhood search, iterated local search, scatter
-search and path relinking, and their hybridisation, such as memetic
-algorithms. Automatic algorithm configuration and design,
-meta-optimisation, model-based methods, and hyperheuristics are also
-topics of interest. Successfully solved problems include, but are not
-limited to, multi-objective, uncertain, dynamic and stochastic
-problems in the context of scheduling, timetabling, network design,
+    <p>The 19th European Conference on Evolutionary Computation in Combinatorial
+Optimisation is a multidisciplinary conference that brings together researchers
+working on evolutionary computation methods and other metaheuristics for
+solving difficult combinatorial optimisation problems appearing in various
+industrial, economic, and scientific domains. Prominent examples of
+metaheuristics include: evolutionary algorithms, estimation of distribution
+algorithms, swarm intelligence methods such as ant colony and particle swarm
+optimisation, local search methods such as simulated annealing, tabu search,
+variable neighbourhood search, iterated local search, scatter search and path
+relinking, and their hybridisation, such as memetic algorithms. Automatic
+algorithm configuration and design, meta-optimisation, model-based methods, and
+hyperheuristics are also topics of interest. Successfully solved problems
+include, but are not limited to, multi-objective, uncertain, dynamic and
+stochastic problems in the context of scheduling, timetabling, network design,
 transportation and distribution, vehicle routing, graph problems,
-satisfiability, energy optimisation, cutting, packing, and planning
-problems.</p>
+satisfiability, energy optimisation, cutting, packing, and planning problems.</p>
 
 
 <p>
-The EvoCOP 2019 conference will be held in the city of Parma, Italy. 
-It will be held in conjunction with EuroGP (the 22nd European
-Conference on Genetic Programming), EvoMUSART (8th European conference on
-evolutionary and biologically inspired music, sound, art and design) and
-EvoApplications (the 22nd European Conference on the Applications of
-Evolutionary Computation), in a joint event collectively known as EvoStar
-(Evo*).
+The EvoCOP 2019 conference will be held in the city of Leipzing, Germany,
+together with EuroGP (the 22nd European Conference on Genetic Programming),
+EvoMUSART (the 8th European conference on evolutionary and biologically inspired
+music, sound, art and design) and EvoApplications (the 22nd European Conference
+on the Applications of Evolutionary Computation), in a joint event collectively
+known as EvoStar (Evo*).
 </p>
 
     <p><strong>Areas of Interest and Contributions</strong></p>
@@ -90,7 +86,9 @@ Evolutionary Computation), in a joint event collectively known as EvoStar
 
     </ul>
 
-
+Notice that, by tradition, continuous/numerical optimisation is *not* part of
+the topics of interest of EvoCOP. Interested authors might consider submitting
+to other EvoStar conferences such as EvoApplications.
       
     <h3>Evo<span class="red">COP</span> Programme Chairs</h3>
 
@@ -119,35 +117,66 @@ TBA
 
 
 
-    <p><strong>Publication Details</strong></p>
-<p>Regular papers will be presented orally at the conference and
-printed in the proceedings published by Springer in the LNCS series
-(see LNCS volumes 2037, 2279, 2611, 3004, 3448, 3906, 4446, 4972,
-5482, 6022, 6622, 7245, 7832, 8600, 9026, 9595 and 10197 for the
-previous proceedings).</p>
+    <h3>Publication Details</h3>
+EvoCOP 2019 will accept three types of submissions:
 
-<p>Late-breaking abstracts (LBAs) summarising ongoing research, recent
-studies and applications of evolutionary computation and other
-meta-heuristics to real-world or academic combinatorial optimisation
-problems. LBAs will be presented as posters during the conference.
+<h4>Regular papers</h4>
+<p>Regular papers will be presented orally at the conference and printed in the
+proceedings published by Springer in the LNCS series (see LNCS volumes 2037,
+2279, 2611, 3004, 3448, 3906, 4446, 4972, 5482, 6022, 6622, 7245, 7832, 8600,
+9026, 9595, 10197 and 10782 for the previous proceedings).</p>
+
+<p>
+The best regular paper presented at EvoCOP 2019 will be distinguished with a
+*Best Paper Award*. Authors of the best regular papers will be invited for fast
+track publication in the *Evolutionary Computation Journal*. The invited
+submissions must significantly extend the conference paper and will undergo
+peer review.
 </p>
 
-    <p><strong>Submission Details</strong></p>
-<p>Regular paper submissions must be original and not published
-elsewhere. The submissions will be peer reviewed by members of the
-program committee. The authors of accepted papers will have to improve
-their paper on the basis of the reviewers' comments and will be asked
-to send a camera ready version of their manuscripts. At least one
-author of each accepted work has to register for the conference,
+
+<h4>Reproducibility studies</h4>
+Reproducibility studies are concise and rigorous experimental studies that
+replicate published experiments, and that substantially shift the confidence in
+the main results of the original study. We strongly encourage the authors to
+discuss the reasons for the new findings, such as, e.g., input data not
+considered in the original work or different implementation details.
+A reproducibility study should follow the usual reproducibility standards, that
+is, it should provide complete details about the implementation, compiler
+options, input data description, algorithmic parameters, operating system and
+hardware specification.  Moreover, the code should be available in a public
+repository immediately after the submission to EvoCOP and should remain
+available after publication in the proceedings. Statistical analysis, if
+applicable, should provide details about the statistical assumptions and
+statistical testing procedures.  Although it is expected that textual
+similarity between the reproducibility study and the original work may arise,
+the usual plagiarism standards will be taken into account.  Reproducibility
+studies will take the form of short papers presented orally at the conference
+and published in the Springer LNCS proceedings.
+
+
+<h4>Late breaking abstracts</h4>
+<p>Late-breaking abstracts (LBAs) summarise ongoing research, recent studies and
+applications of evolutionary computation and other meta-heuristics to real-world
+or academic combinatorial optimisation problems. LBAs will be presented as short
+talks during the conference.
+</p>
+
+    <h3>Submission Details</h3>
+<p>Paper submissions must be original and not published elsewhere. The submissions
+will be peer reviewed by members of the program committee. The authors of
+accepted papers will have to improve their paper on the basis of the reviewers'
+comments and will be asked to send a camera ready version of their manuscripts.
+At least one author of each accepted work has to register for the conference,
 attend the conference and present the work.</p>
 <p>
-The reviewing process will be double-blind, please omit information
-about the authors in the submitted paper. Submit your manuscript in
-Springer LNCS format.
+The reviewing process will be double-blind, please omit information about the
+authors in the submitted paper. Submit your manuscript in Springer LNCS format.
 </p>
 <ul>
 <li>Page limit (Regular papers): 16 pages</li>
-<li>Page limit (LBAs): 1 page</li>
+<li>Page limit (Reproducibility studies): 8 pages</li>
+<li>Page limit (LBAs): 1-2 pages</li>
 </ul>
 
 <p><strong>Submission link (Regular papers)</strong>: <a href="https://myreview.saclay.inria.fr/evocop19/">https://myreview.saclay.inria.fr/evocop19/</a><br /></p>
