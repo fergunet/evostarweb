aab9520a351521587077632377e7c4d6ba346c70
Pablo García Sánchez <fergunet@gmail.com>
2018-07-30 13:58:37 +0200

    Updated Evoapps

--- cfp_evoapps.php
+++ cfp_evoapps.php
@@ -3,20 +3,21 @@
         <h2>Evo<span class="red">Applications</span><h2>
    </div>
 
-<h2>21st International Conference on the Applications of Evolutionary Computation</h2>
+<h2>22nd International Conference on the Applications of Evolutionary Computation</h2>
 
 <?php include('common_to_all.php') ?>
+<!--
 <ul>
 <li><a href="#abstracts">Accepted paper abstracts</a></li>
 </ul>
-
+-->
  <p>Evo<span class="red">Applications</span></a></span>, 
       the International Conference on the Applications of Evolutionary Computation 
       -formerly known as EvoWorkshops- brings together researchers in a variety 
       of areas of application of Evolutionary Computation and other Nature-inspired 
       techniques. </p>
     <p> <span id="logo">Evo<span class="red">Applications</span>
-      invites high quality contributions for its 21st edition, which will be held 
+      invites high quality contributions for its 22nd edition, which will be held 
       as part of the Evo<span class="red">Star</span>
       2019 event in Leipzig, Germany, April 2019 and co-located within Evo<span class="red">Star</span> 
       with three related conferences: <a href="cfp_eurogp.html">Euro<span class="red">GP</span></a>, <a href="cfp_evocop.html">Evo<span class="red">COP</span></a> 
@@ -26,7 +27,7 @@
      <!-- <strong><p>An extended version of the EvoAPPLICATIONS best papers will be published in a special issue of <a href="https://link.springer.com/journal/12293">Memetic Computing</a> (Impact Factor: 1.00)</p></strong>
   -->
     <p><span id="logo">Evo<span class="red">Applications</span>
-      will focus on 9 Thematic Areas: </span> </p>
+      will focus on the following Thematic Areas: </span> </p>
     <span id="logo"> 
 <ul>
 <li>Games</li>
@@ -43,23 +44,18 @@
  <?php include('common_to_apps_publication.php') ?> 
 
  
-    <h3> FURTHER INFORMATION </h3>
-    <p> Further information on the conference and co-located events can be <br />
-      found in: <a href="http://www.evostar.org/">http://www.evostar.org</a> </p>
     <span id="logo"> 
-    <h3>Evo<span class="red">Applications</span> 
+    <h3>Evo<span class="red">Publication</span> 
       coordinator</h3>
     <blockquote> 
-      <p><span><strong>Kevin Sim</strong> <br />
-        Edinburgh Napier University,  Scotland<br />
-        <em>k.sim (at) napier.ac.uk </em></span></p>
+      <p><span><strong>Paul Kaufmann </strong> <br />
+       Mainz University, Germany<br />
+        <em>paul.kaufmann (at) uni-mainz.de </em></span></p>
     </blockquote>
 
 <h3>Evo<span class="red">Applications</span> Publication Chair</h3>
     <blockquote> 
-      <p><span><strong>Paul Kaufmann </strong> <br />
-       Mainz University, Germany<br />
-        <em>paul.kaufmann (at) gmail.com </em></span></p>
+TBA
     </blockquote>
 
 
