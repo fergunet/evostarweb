77dd6c926f19f27b088711eec725079b0b8d413f
Pablo García Sánchez <fergunet@gmail.com>
2018-08-22 13:44:15 +0200

    Added information about leipzig

--- location_about_leipzig.php
+++ location_about_leipzig.php
@@ -3,5 +3,24 @@
         <h2>About <span class="red">Leipzig</span></h2>
    </div>
 
-TBA
+<p>
+Leipzig is located in the north-east of Germany, about 160km south of Berlin. It is the largest city in
+the federal state of Saxony and looks back at more than 1000 years of history. Situated at the
+crossing of two major medieval trading routes, Via Imperii connecting Italy with the North and Via
+Regia connecting East and West, it was once considered as one of the major European centers of
+trade, culture and learning. Following the reunification in 1990, Leipzig reestablished itself as an
+important city of commerce, arts and science in Germany.
+Today Leipzig is an easily likeable, green and young city with a rich cultural diversity. Particularly in
+the city center we find many beautiful historical buildings from different architectural periods,
+museums and parks. Also, lovers of classical music appreciate Leipzig due to the impact of Bach and
+Mendelsohn-Bartholdy, who lived in the city for important parts of their lives.
+</p>
+Further information about Leipzig:
+<ul>
+<li><a href="https://en.wikipedia.org/wiki/Leipzig">https://en.wikipedia.org/wiki/Leipzig</a></li>
+
+<li><a href="https://english.leipzig.de/leisure-culture-and-tourism/tourism/">https://english.leipzig.de/leisure-culture-and-tourism/tourism/</a></li>
+</ul>
+See a short video about Things To Do In Leipzig at
+<a href="https://www.youtube.com/watch?v=rG8k-MgtmPc">https://www.youtube.com/watch?v=rG8k-MgtmPc</a>
  <?php include('footer.php') ?>
