fe5bfe5436e49615d6a9b8592116298342bd5018
Pablo García Sánchez <fergunet@gmail.com>
2018-09-27 23:40:00 +0200

    Added chair again

--- cfp_evoapps.php
+++ cfp_evoapps.php
@@ -63,14 +63,14 @@
 <h3>Evo<span class="red">Apps</span> area chairs</h3>
 <ul>
 <li> <span class="red">Engineering + Real world applications:</span>  Emma Hart, Sara Silva</li>
-<li> <span class="red">Games: </span>Alberto Tonda </li>
-<li> <span class="red">Image & Signal Processing, Vision & Pattern recognition:</span> Stephen Smith and Monica Mordonini </li>
+<li> <span class="red">Games: </span>Julian Togelius, Alberto Tonda </li>
+<li> <span class="red">Image & Signal Processing, Vision & Pattern recognition:</span> Stephen Smith, Monica Mordonini </li>
 <li> <span class="red">Life sciences:</span> James Foster, Jaume Bacardit</li>
 <li> <span class="red">Networks:</span> JJ Merelo, Juan Luis (Juanlu) Jiménez-Laredo</li>
 <li> <span class="red">Neuroevolution:</span> Julian Miller, Sebastian Risi</li>
-<li> <span class="red">Numerical optimization:</span> Günther Rudolf  and Oliver Kramer</li>
+<li> <span class="red">Numerical optimization:</span> Günther Rudolf, Oliver Kramer</li>
 <li> <span class="red">Robotics:</span> Gusz Eiben, Kyrre Glette</li>
-<li> <span class="red">General:</span> Carlos Cotta, Paco Fernandez, Giovanni Iacca and Trung Thanh Nguyen</li>
+<li> <span class="red">General:</span> Carlos Cotta, Paco Fernandez, Giovanni Iacca, Trung Thanh Nguyen</li>
 </ul>
 
 
