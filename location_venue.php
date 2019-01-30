17f5aef49ef2fbd95a788f08ab5e1094b4598bca
Pablo García Sánchez <fergunet@gmail.com>
2019-01-30 21:39:27 +0100

    Venue information added

--- location_venue.php
+++ location_venue.php
@@ -5,6 +5,20 @@
    </div>
 
 
-TBA
+Location address:
+
+<blockquote>
+HTWK Leipzig<br/>
+Faculty of Electrical Engineering and Information Technology<br/>
+Wächterstraße 13<br/>
+04251 Leipzig
+</blockquote>
+
+<img class="img-responsive" src="img/leipzig_venue.jpg" />
+Photo by <a href="https://de.wikipedia.org/wiki/Technische_Hochschule_Leipzig#/media/File:Leipzig,_W%C3%A4chterstra%C3%9Fe_13.jpg">Reinhard Ferdinand (Creative Commons)</a>
+<p>
+<!--A detailed route description can be found <a href="">here </a>-->
+</p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.8048056041844!2d12.363941315763585!3d51.33310997960693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6f8283152ecb1%3A0xdf0dcff5b8488dcc!2sHTWK+Leipzig+-+Fakult%C3%A4t+Elektrotechnik+und+Informationstechnik!5e0!3m2!1ses!2ses!4v1548879716614" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
     <?php include('footer.php') ?>
