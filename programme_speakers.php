2b61336dd2f480c163398647850e030bc07eaa32
Pablo García Sánchez <fergunet@gmail.com>
2019-02-05 20:43:20 +0100

    Changed keynotes2

--- programme_speakers.php
+++ programme_speakers.php
@@ -4,9 +4,35 @@
    </div>
 
 
+
 <h3>Opening talk: TBA</h3>
-<h4> Manja Marz</h4>
-<h4> Date: TBA</h4>
+
+<h4>Risto Miikkulainen, University of Texas, USA</h4>
+<h4>Date: 24 April, 09:45</h4>
+
+<div>
+<img src="img/keynotes/miikkulainen-hr.jpg" width="200px" align="right" style="margin:10px"/>
+<strong>Abstract: </strong>TBA
+<p>
+<strong>Bio: </strong> 
+Risto Miikkulainen is a Professor of Computer Science at the
+University of Texas at Austin and Associate VP of Evolutionary AI at
+Cognizant. He received an M.S. in Engineering from Helsinki University
+of Technology (now Aalto University) in 1986, and a Ph.D. in Computer
+Science from UCLA in 1990.  His current research focuses on methods
+and applications of neuroevolution, as well as neural network models
+of natural language processing and vision; he is an author of over 400
+articles in these research areas. Risto is an IEEE Fellow, and his work
+on neuroevolution has recently been recognized with the Gabor Award of
+the International Neural Network Society and Outstanding Paper of the
+Decade Award of the International Society for Artificial Life.
+</p>
+</div>
+
+
+<h3>Closing talk: TBA</h3>
+<h4> Manja Marz, Friedrich Schiller University Jena, Germany</h4>
+<h4> Date: 26 April, 12:00</h4>
 
 
 <div>
@@ -29,27 +55,4 @@ Championship in 2017.
 </br>
 </br>
 <hr/>
-<h3>Closing talk: TBA</h3>
-
-<h4>Risto Miikkulainen, University of Texas, USA</h4>
-<h4>Date: TBA</h4>
-
-<div>
-<img src="img/keynotes/miikkulainen-hr.jpg" width="200px" align="right" style="margin:10px"/>
-<strong>Abstract: </strong>TBA
-<p>
-<strong>Bio: </strong> 
-Risto Miikkulainen is a Professor of Computer Science at the
-University of Texas at Austin and Associate VP of Evolutionary AI at
-Cognizant. He received an M.S. in Engineering from Helsinki University
-of Technology (now Aalto University) in 1986, and a Ph.D. in Computer
-Science from UCLA in 1990.  His current research focuses on methods
-and applications of neuroevolution, as well as neural network models
-of natural language processing and vision; he is an author of over 400
-articles in these research areas. Risto is an IEEE Fellow, and his work
-on neuroevolution has recently been recognized with the Gabor Award of
-the International Neural Network Society and Outstanding Paper of the
-Decade Award of the International Society for Artificial Life.
-</p>
-</div>
        <?php include('footer.php') ?>
