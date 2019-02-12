59215f033c642b547c10810af3e8a81db91787d7
Pablo García Sánchez <fergunet@gmail.com>
2019-02-12 13:38:45 +0100

    added programme and best paper award

--- programme_bestpapers.php
+++ programme_bestpapers.php
@@ -3,7 +3,53 @@
 
         <h2>Best Paper <span class="red">Candidates</span> <h2>
    </div>
-TBA
+<h3>Euro<span class="red">GP</span></h3>
+<ul>
+    <li><strong>Can Genetic Programming Do Manifold Learning Too?</strong>
+    Andrew Lensen, Bing Xue, Mengjie Zhang
+
+    <li><strong>Why is Auto-Encoding Difficult for Genetic Programming?</strong>
+    James McDermott</li>
+
+    <li><strong>Towards a Scalable EA-based Optimization of Digital Circuits</strong>
+    Jitka Kocnova, Zdenek Vasicek</li>
+</ul>
+<h3>Evo<span class="red">APPS</span></h3>
+<ul>
+    <li><strong>Particle Swarm Optimization: Understanding Order-2 Stability Guarantees</strong>
+    Christopher Cleghorn</li>
+
+    <li><strong>Supporting Medical Decisions for Treating Rare Diseases through Genetic Programming</strong>
+    Illya Bakurov, Leonardo Vanneschi, Mauro Castelli, Maria João Freitas</li>
+
+    <li><strong>Evolutionary Algorithms for the Design of Quantum Protocols</strong>
+    Walter Krawec, Stjepan Picek, Domagoj Jakobovic</li>
+
+</ul>
+<h3>Evo<span class="red">COP</span></h3>
+<ul>
+
+    <li><strong>Clarifying the Differences in Local Optima Network Sampling Algorithms, </strong>
+    Sarah L. Thomson, Gabriela Ochoa, Sébastien Verel</li>
+
+
+    <li><strong>An Iterated Local Search Algorithm for the Two-Machine Flow Shop Problem with Buffers and Constant Processing Times on One Machine, </strong>
+    Hoang Thanh Le, Philine Geser, Martin Middendorf</li>
+
+    <li><strong>Quasi-Optimal Recombination Operator, </strong>
+    Francisco Chicano, Gabriela Ochoa, Darrell Whitley, Renato Tinós</li>
+
+
+</ul>
+<h3>Evo<span class="red">MUSART</span></h3>
+<ul>
+
+    <li><strong>Swarm-based identification of animation key points from 2D-medialness maps</strong>
+    Prashant Aparajeya, Frederic Fol Leymarie, Mohammad Majid al-Rifaie</li>
+
+    <li><strong>Paintings, Polygons and Plant Propagation  </strong>
+    Misha Paauw, Daan van den Berg </li>
+</ul>
 
 
 
