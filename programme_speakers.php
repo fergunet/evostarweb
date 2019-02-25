37985ba89307384fc703e03ec6162e95aaf1dfec
Pablo García Sánchez <fergunet@gmail.com>
2019-02-25 11:49:53 +0100

    Added abstract for keynotes

--- programme_speakers.php
+++ programme_speakers.php
@@ -5,14 +5,28 @@
 
 
 
-<h3>Opening talk: TBA</h3>
+<h3>Opening talk: Creative AI through Evolutionary Computation</h3>
 
 <h4>Risto Miikkulainen, University of Texas, USA</h4>
 <h4>Date: 24 April, 09:45</h4>
 
 <div>
 <img src="img/keynotes/miikkulainen-hr.jpg" width="200px" align="right" style="margin:10px"/>
-<strong>Abstract: </strong>TBA
+<strong>Abstract: </strong>
+<p>
+Last decade has seen tremendous progress in Artificial Intelligence
+(AI). AI is now in the real world, powering applications that have a
+large practical impact. Most of it is based on modeling what is
+already known, e.g. predicting what the right classification of an
+image or a language sequence might be.  The next step for AI is
+machine creativity, e.g. designing engineering solutions are more
+complex, perform better, or at a lower cost than existing solutions.
+Evolutionary computation is likely to play a central role in future
+such AI.  I will review several recent techniques and applications
+where evolutionary creativity improves upon best human solutions,
+including designing neural network architectures, web interfaces, and
+growth recipes for agriculture.
+</p>
 <p>
 <strong>Bio: </strong> 
 Risto Miikkulainen is a Professor of Computer Science at the
@@ -30,14 +44,29 @@ Decade Award of the International Society for Artificial Life.
 </div>
 
 
-<h3>Closing talk: TBA</h3>
+<h3>Closing talk: The discovery of RNA evolution with long read sequencing methods</h3>
 <h4> Manja Marz, Friedrich Schiller University Jena, Germany</h4>
 <h4> Date: 26 April, 12:00</h4>
 
 
 <div>
 <img src="img/keynotes/manja-marz.jpg" width="200px" align="left" style="margin:10px"/>
-<strong>Abstract:</strong> TBA
+<strong>Abstract:</strong>
+
+<p>
+	Since more than a decade it is known that RNAs play a major role in
+basically all molecular processes o any cell.  However the evolution
+of RNAs is not so easy to trace back due to their selection pressure
+on secondary structures instead of the nucleic acid sequence itself.
+In this talk we will understand the evolution of well known examples
+of conserved RNAs. But there have been a lot of mysterious RNA
+molecules of unknown shape and origin, which we will investigate
+during the talk by using newest sequencing methods, such as ONT
+Minion.
+This direct RNA sequencing technique reveals very long reads, which
+make it also possible to discover the evolution of whole genomes from
+RNA viruses.
+</p>
 <p>
 <strong>Bio:</strong> Dr. Manja Marz is Full Professor for High Throughput Sequencing Analysis at the Faculty of
 Mathematics and Computer Science of the Friedrich Schiller University Jena, Germany and managing
