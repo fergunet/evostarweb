1c07e40639090b1f92099cf196dd095c4e8085b6
Pablo García Sánchez <fergunet@gmail.com>
2019-03-14 20:03:27 +0100

    Programme future

---
+++ programme_future.php
@@ -0,0 +1,16 @@
+<?php include('header.php') ?>
+
+        <h2>Future and Emerging Technologies Session<h2>
+   </div>
+
+
+
+
+<h3>Thursday 25th April 9:30</h3>
+<p>
+In this session representatives from the European Commission will present the 2019 FET-related funding opportunities, both from FET-Open and FET-Proactive and within the new European Innovation Council (EIC) pilot.
+</p>
+<p>
+Two FET researchers will also share their experience and provide advice on the FET programme.
+</p>   
+       <?php include('footer.php') ?>
