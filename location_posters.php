17680271413cae6a71bd46c9d8acf7d5866b5ef5
Pablo García Sánchez <fergunet@gmail.com>
2019-04-02 22:50:27 +0200

    Added poster info

--- location_posters.php
+++ location_posters.php
@@ -2,5 +2,27 @@
 
         <h2>Posters/<span class="red">Wifi</span>:<h2>
    </div>
-TBA
+<h3>AV at the venue</h3>
+You are encouraged to bring your own laptop or tablet for your presentation, along with appropriate adaptors and chargers for your equipment as EvoStar does not provide these. All LCD projectors (beamers) have a VGA input. Some may provide HDMI, but this is not guaranteed. Voltage in Germany is 220v, and the two-pin plug is used, similar to most European countries.
+<h3>POSTERS</h3>
+<p>
+The poster session will take place in conjunction with the EvoStar reception on Wednesday evening, 24 April from 18:30-20:00. Poster boards are 1.20 m wide by 1.50 m tall and can accommodate A0-size posters. Tape can be used to fix posters and this will be available at the conference venue.
+</p>
+Poster tubes can be stored safely at the registration desk, but please remember to label your poster so it does not get lost or mixed up with others.
+
+<p>
+There is a print shop near the venue where you can have your poster printed:
+</p>
+
+sedruck - https://www.sedruck-leipzig.de/<br/>
+An der Albertina<br/>
+Beethovenstrasse 23<br/>
+04107 Leipzig<br/>
+
+<strong>Prices:</strong><br/>
+DIN A1   15,50 €<br/>
+DIN A0   26,00 €<br/>
+
+<h3>WIFI</h3>
+WIFI is available at the conference venue. In addition to Eduroam being suitable for many, participants will be able to use the guest wifi on the HTWK network. 
    <?php include('footer.php') ?>
