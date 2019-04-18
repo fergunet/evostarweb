6351b3a4ab635adf772235d4a066eeeb4687f659
Pablo García Sánchez <fergunet@gmail.com>
2019-04-18 16:02:44 +0200

    Added new version of the handbook and URL for proceedings

--- proceedings.php
+++ proceedings.php
@@ -2,15 +2,13 @@
 
         <h2>Download <span class="red">Proceedings</span><h2>
    </div>
-TBA
-<!--
-Temporary free access to the online version of the EvoStar 2015 conference proceedings on SpringerLink are available for registered conference participants of EvoStar 2015 from <strong>1 April to 1 May 2015</strong>.
+
+Temporary free access to the online version of the EvoStar 2019 conference proceedings on SpringerLink are available for registered conference participants of EvoStar 2015 from <strong>1 April to 1 May 2015</strong>.
 <ul>
-<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16501-1">Euro<span class="red">GP</span> 2015, LNCS 9025</a></li>
-<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16468-7">Evo<span class="red">COP</span> 2015, LNCS 9026</a></li>
-<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16498-4">Evo<span class="red">MUSART</span> 2015, LNCS 9027</a></li>
-<li><a href="http://link.springer.com/book/10.1007%2F978-3-319-16549-3">Evo<span class="red">Applications</span> 2015, LNCS 9028</a></li>
+<li><a href="https://www.springer.com/gp/book/9783030166694">Euro<span class="red">GP</span> 2019</a></li>
+<li><a href="https://link.springer.com/book/10.1007/978-3-030-16692-2">Evo<span class="red">APPS</span> 2019</a></li>
+<li><a href="https://link.springer.com/book/10.1007/978-3-030-16667-0">Evo<span class="red">MUSART</span> 2019</a></li>
 </ul>
--->
+
           <?php include('footer.php') ?>
 
