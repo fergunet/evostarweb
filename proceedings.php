24a188e5522413b753935a0fb54144ce7c740c22
Pablo García Sánchez <fergunet@gmail.com>
2019-04-23 12:12:36 +0200

    Changed evocop link

--- proceedings.php
+++ proceedings.php
@@ -7,12 +7,12 @@ Temporary free access to the online version of the EvoStar 2019 conference proce
 <ul>
 <li><a href="https://www.springer.com/gp/book/9783030166694">Euro<span class="red">GP</span> 2019</a></li>
 <li><a href="https://link.springer.com/book/10.1007/978-3-030-16692-2">Evo<span class="red">APPS</span> 2019</a></li>
-<li><a href="https://www.springer.com/978-3-030-16710-3">Evo<span class="red">COP</span> 2019</a></li>
+<li><a href="https://link.springer.com/book/10.1007/978-3-030-16711-0">Evo<span class="red">COP</span> 2019</a></li>
 <li><a href="https://link.springer.com/book/10.1007/978-3-030-16667-0">Evo<span class="red">MUSART</span> 2019</a></li>
 
 </ul>
 
-Note: EuroGP and EvoCOP will be available soon.
+Note: EuroGP will be available soon.
 
           <?php include('footer.php') ?>
 
